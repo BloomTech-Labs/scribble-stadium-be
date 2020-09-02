9bddfc72f32defb295d017bf06663c5758af3e56
Brandon Ramirez <bran.ramirez.don@gmail.com>
2020-09-02 16:51:26 -0700

    Created migration for initial DB Schema. Potentially need to adjust Auth field on parents and PINs on children and parents.

---
+++ data/migrations/20200902161643_new_schema.js
@@ -0,0 +1,46 @@
+exports.up = function (knex) {
+  return knex.schema
+    .createTable('Parents', (t) => {
+      t.increments('ID');
+      t.string('Name').notNullable();
+      t.string('Email').notNullable().unique();
+      t.string('PIN').notNullable();
+      t.string('Auth');
+    })
+    .createTable('Avatars', (t) => {
+      t.increments('ID');
+      t.string('AvatarURL').notNullable().unique();
+    })
+    .createTable('Children', (t) => {
+      t.increments('ID');
+      t.string('Name').notNullable();
+      t.string('PIN').notNullable();
+      t.integer('AvatarID')
+        .notNullable()
+        .unsigned()
+        .references('Avatars.ID')
+        .onUpdate('CASCADE')
+        .onDelete('RESTRICT');
+      t.integer('ParentID')
+        .notNullable()
+        .unsigned()
+        .references('Parents.ID')
+        .onUpdate('CASCADE')
+        .onDelete('RESTRICT');
+    })
+    .createTable('Stories', (t) => {
+      t.increments('ID');
+      t.string('Title').notNullable();
+      t.string('URL').notNullable().unique();
+      t.string('WritingPrompt').notNullable();
+      t.string('DrawingPrompt').notNullable();
+    });
+};
+
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists('Stories')
+    .dropTableIfExists('Children')
+    .dropTableIfExists('Avatars')
+    .dropTableIfExists('Parents');
+};
