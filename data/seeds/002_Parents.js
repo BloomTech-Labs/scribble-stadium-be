2db98c03743dd18f97efa5f59994ccb3bc27692a
Brandon Ramirez <bran.ramirez.don@gmail.com>
2020-09-03 16:35:50 -0700

    Updated parent seeds with test emails.

--- data/seeds/002_Parents.js
+++ data/seeds/002_Parents.js
@@ -4,7 +4,7 @@ const bc = require('bcryptjs');
 const parents = [...new Array(5)].map((i, idx) => ({
   Name: `${faker.name.firstName()} ${faker.name.lastName()}`,
   PIN: `${bc.hashSync(`000${idx}`, process.env.BCRYPT_ROUNDS || 6)}`,
-  Email: faker.internet.email(),
+  Email: `llama00${idx + 1}@maildrop.cc`,
 }));
 
 exports.seed = function (knex) {
