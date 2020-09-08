744861074f31b085198b5efda5924a1a4826e769
Brandon Ramirez <bran.ramirez.don@gmail.com>
2020-09-07 20:25:03 -0700

    Implemented boilerplate code for parent data model, router, and tests.

---
+++ api/parent/parentRouter.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const authRequired = require('../middleware/authRequired');
+const Parents = require('./parentModel');
+const router = express.Router();
+
+module.exports = Router;
