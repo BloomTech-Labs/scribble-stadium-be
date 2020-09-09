4ac9f17427b5104caff0a2a070d9b0dd1021f417
Brandon Ramirez <bran.ramirez.don@gmail.com>
2020-09-08 22:13:12 -0700

    Finished queries and models for parent data.

--- api/parent/parentModel.js
+++ api/parent/parentModel.js
@@ -1,19 +1,29 @@
 const db = require('../../data/db-config');
 
-const findAll = async () => {
-  return await db('Parents');
+const findAll = () => {
+  return db('Parents');
 };
 
-const findById = async (ID) => {
-  return await db('Parents').where({ ID });
+const findById = (ID) => {
+  return db('Parents').where({ ID });
 };
 
-const add = async (parent) => {
-  return await db('Parents').insert(parent);
+const add = (parent) => {
+  return db('Parents').insert(parent).returning('ID');
+};
+
+const update = (ID, changes) => {
+  return db('Parents').where({ ID }).update(changes);
+};
+
+const remove = (ID) => {
+  return db('Parents').where({ ID }).del();
 };
 
 module.exports = {
   findAll,
   findById,
   add,
+  update,
+  remove,
 };
--- api/parent/parentRouter.js
+++ api/parent/parentRouter.js
@@ -19,10 +19,10 @@ router.get('/', authRequired, async (req, res) => {
  *
  */
 router.get('/:id', authRequired, async (req, res) => {
-  const { id } = req.body;
+  const { id } = req.params;
   try {
     const parent = await Parents.findById(id);
-    if (parent) {
+    if (parent.length > 0) {
       res.status(200).json(parent);
     } else {
       res.status(404).json({ error: 'ParentNotFound' });
@@ -35,12 +35,46 @@ router.get('/:id', authRequired, async (req, res) => {
 /**
  *
  */
-
 router.post('/', authRequired, async (req, res) => {
   const parent = req.body;
   try {
     const data = await Parents.add(parent);
     res.status(201).json(data);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+/**
+ *
+ */
+router.put('/:id', authRequired, async (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+  try {
+    const count = await Parents.update(id, changes);
+    if (count > 0) {
+      res.status(202).end();
+    } else {
+      res.status(404).json({ error: 'ParentNotFound' });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+/**
+ *
+ */
+router.delete('/:id', authRequired, async (req, res) => {
+  const { id } = req.params;
+  try {
+    const count = await Parents.remove(id);
+    if (count > 0) {
+      res.status(202).end();
+    } else {
+      res.status(404).json({ error: 'ParentNotFound' });
+    }
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
