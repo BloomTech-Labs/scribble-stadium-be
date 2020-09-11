c2f9bc1e3b012c0bd40f21789ce0fcdbebfcfc67
Brandon Ramirez <bran.ramirez.don@gmail.com>
2020-09-11 11:27:17 -0700

    Documented GET /children with OpenAPI

--- api/child/childRouter.js
+++ api/child/childRouter.js
@@ -2,6 +2,41 @@ const router = require('express').Router();
 const authRequired = require('../middleware/authRequired');
 const Children = require('./childModel');
 
+/**
+ * @swagger
+ * /children:
+ *  get:
+ *    description: Returns a list of children
+ *    summary: Get a list of all child profiles
+ *    security:
+ *      - okta: []
+ *    tags:
+ *      - child
+ *    responses:
+ *      200:
+ *        description: array of all children
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                $ref: '#/components/schemas/Child'
+ *              example:
+ *                - ID: 1
+ *                  Name: 'Alison Brie'
+ *                  PIN: '1jkkj0f89n2083n9fnq23rbf'
+ *                  AvatarID: 1
+ *                  ParentID: 1
+ *                - ID: 2
+ *                  Name: 'Gillian Jacobs'
+ *                  PIN: '1jkkj0f89n2083n9fnq23rba'
+ *                  AvatarID: 2
+ *                  ParentID: 1
+ *      401:
+ *        $ref: '#/components/responses/UnauthorizedError'
+ *      500:
+ *        $ref: '#/components/responses/DatabaseError'
+ */
 router.get('/', authRequired, async (req, res) => {
   try {
     const children = await Children.findAll();
@@ -11,6 +46,7 @@ router.get('/', authRequired, async (req, res) => {
   }
 });
 
+/** */
 router.get('/:id', authRequired, async (req, res) => {
   const { id } = req.params;
   try {
