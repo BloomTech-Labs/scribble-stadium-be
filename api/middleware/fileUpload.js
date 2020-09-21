c8bc75e12c4e0253dc571d26b82d6fee722b468c
Brandon Ramirez <bran.ramirez.don@gmail.com>
2020-09-20 18:57:12 -0700

    Created middleware to handle file uploads. Might need more work when connecting with an antdesign form.

---
+++ api/middleware/fileUpload.js
@@ -0,0 +1,59 @@
+const multiparty = require('multiparty');
+const fileType = require('file-type');
+const fs = require('fs');
+
+const { uploadFile } = require('../../lib/awsBucket');
+
+const fileUploadHandler = (req, res, next) => {
+  const form = new multiparty.Form();
+  form.parse(req, async (error, fields, files) => {
+    if (error) throw new Error(error);
+    if (files.length === 0) res.status(400).json({ error: 'NoFileGiven' });
+
+    try {
+      const fileNames = Object.keys(files);
+      const fileList = fileNames.map(async (f) => {
+        const path = files[f][0].path;
+        const buffer = fs.readFileSync(path);
+        const type = await fileType.fromBuffer(buffer);
+        const timestamp = Date.now().toString();
+        const fileName = `bucketFolder/${timestamp}-lg`;
+        return uploadFile(buffer, fileName, type);
+      });
+
+      const fileURLs = await Promise.all(fileList);
+
+      const fileObjects = {};
+      fileURLs.forEach((url, idx) => {
+        fileObjects[fileNames[idx]] = url;
+      });
+
+      console.log(fileObjects);
+      req.files = fileObjects;
+
+      // Continue if all things pass
+      next();
+    } catch (error) {
+      res.status(500).json({ error: 'FileUploadFailed' });
+    }
+
+    // try {
+    //   const path = files.avatar[0].path;
+    //   const buffer = fs.readFileSync(path);
+    //   const type = await fileType.fromBuffer(buffer);
+    //   const timestamp = Date.now().toString();
+    //   const fileName = `bucketFolder/${timestamp}-lg`;
+
+    //   // Call our S3 upload function
+    //   const { Location } = await uploadFile(buffer, fileName, type);
+
+    //   return res.status(200).send(Location);
+    // } catch (error) {
+    //   return res.status(400).json(error);
+    // }
+  });
+};
+
+module.exports = {
+  fileUploadHandler,
+};
--- api/upload/uploadRouter.js
+++ api/upload/uploadRouter.js
@@ -1,30 +1,11 @@
 const router = require('express').Router();
-const multiparty = require('multiparty');
-const fileType = require('file-type');
-const fs = require('fs');
 
-const { uploadFile } = require('../../lib/awsBucket');
+const { fileUploadHandler } = require('../middleware/fileUpload');
 
-router.post('/', async (req, res) => {
-  const form = new multiparty.Form();
-  form.parse(req, async (error, fields, files) => {
-    if (error) throw new Error(error);
-    res.send({ files, fields });
-    // try {
-    //   const path = files.file[0].path;
-    //   const buffer = fs.readFileSync(path);
-    //   const type = await fileType.fromBuffer(buffer);
-    //   const timestamp = Date.now().toString();
-    //   const fileName = `bucketFolder/${timestamp}-lg`;
+router.post('/', fileUploadHandler, async (req, res) => {
+  const { files } = req;
 
-    //   // Call our S3 upload function
-    //   const data = await uploadFile(buffer, fileName, type);
-    //   console.log(data);
-    //   return res.status(200).send(data);
-    // } catch (error) {
-    //   return res.status(400).json(error);
-    // }
-  });
+  res.status(200).json({ files });
 });
 
 module.exports = router;
