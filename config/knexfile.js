1e5f52552ec3814bea50a9f7713d4e935820214b
Brandon Ramirez <bran.ramirez.don@gmail.com>
2020-09-28 10:22:53 -0700

    Added changes to make it much easier to run migrations on the CI database.

--- config/knexfile.js
+++ config/knexfile.js
@@ -20,6 +20,13 @@ module.exports = {
     seeds: { directory: '../data/seeds' },
   },
 
+  ci: {
+    client: 'pg',
+    connection: process.env.CI_DATABASE_URL,
+    migrations: { directory: '../data/migrations' },
+    seeds: { directory: '../data/seeds' },
+  },
+
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
