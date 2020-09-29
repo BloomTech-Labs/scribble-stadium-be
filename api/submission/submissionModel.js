8754067614c9eb28ab42d84f83c1c28f5e944665
Brandon Ramirez <bran.ramirez.don@gmail.com>
2020-09-29 14:56:36 -0700

    Added JSDoc notation to the model function for clarity.

--- api/submission/submissionModel.js
+++ api/submission/submissionModel.js
@@ -24,11 +24,53 @@ const getOrInitSubmission = async (ChildID, StoryID) => {
  * @param {number} ID the ID of the submission to be marked as read
  * @returns {Promise} returns a promise that resolves to the count of fields updated
  */
-const markAsRead = (ID) => {
-  return db('Submissions').where({ ID }).update({ HasRead: true });
+const markAsRead = (ID, flag = true) => {
+  return db('Submissions').where({ ID }).update({ HasRead: flag });
+};
+
+/**
+ * This function marks the submission with the given ID as written.
+ * @param {number} ID the ID of the submission to be marked as written
+ * @returns {Promise} returns a promise that resolves to the count of fields updated
+ */
+const markAsWritten = (ID, flag = true) => {
+  return db('Submissions').where({ ID }).update({ HasWritten: flag });
+};
+
+/**
+ * This function marks the submission with the given ID as drawn.
+ * @param {number} ID the ID of the submission to be marked as drawn
+ * @returns {Promise} returns a promise that resolves to the count of fields updated
+ */
+const markAsDrawn = (ID, flag = true) => {
+  return db('Submissions').where({ ID }).update({ HasDrawn: flag });
+};
+
+/**
+ * This function uploads a child's written pages to the database.
+ * @param {Array} pages an array of pages formatted by the router
+ * @returns {Promise} returns a promise that resolves to an array of the newly uploaded
+ * pages with the fields ID, URL, and PageNum
+ */
+const submitWriting = (pages) => {
+  return db('Writing').insert(pages).returning(['ID', 'URL', 'PageNum']);
+};
+
+/**
+ * This function uploads a child's drawing to the database.
+ * @param {Array} drawing an array containing a single drawing formatted by the router
+ * @returns {Promise} returns a promise that resolves to an array containing a single
+ * drawing object with the fields ID and URL
+ */
+const submitDrawing = (drawing) => {
+  return db('Drawing').insert(drawing).returning(['ID', 'URL']);
 };
 
 module.exports = {
   getOrInitSubmission,
   markAsRead,
+  markAsWritten,
+  markAsDrawn,
+  submitWriting,
+  submitDrawing,
 };
