1878f72790ca7e8cbfdc63a22db570fa5086d6a5
Brandon Ramirez <bran.ramirez.don@gmail.com>
2020-10-01 14:19:27 -0700

    Update submissionRouter.js

--- api/submission/submissionRouter.js
+++ api/submission/submissionRouter.js
@@ -281,6 +281,7 @@ router.post('/draw/:id', authRequired, fileUploadHandler, async (req, res) => {
 
     res.status(201).json(submitted);
   } catch ({ message }) {
+    console.log(message);
     if (message.includes('violates foreign key constraint')) {
       res.status(404).json({ error: 'InvalidSubmissionID' });
     } else if (message.includes('violates unique constraint')) {
