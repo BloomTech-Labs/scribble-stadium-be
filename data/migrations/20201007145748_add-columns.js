65aece2be2f9e40098cf18d52b8760203139d1eb
Brandon Ramirez <bran.ramirez.don@gmail.com>
2020-10-07 15:55:59 -0700

    Added new migrations to account for new mdoeration paradigm

--- data/migrations/20201007145748_add-columns.js
+++ data/migrations/20201007145748_add-columns.js
@@ -1,10 +1,19 @@
 exports.up = function (knex) {
   return knex.schema
     .table('Submissions', (t) => {
-      t.enu('Status', ['CLEAR', 'PENDING', 'APPROVED'], {
+      t.enu('Status', ['CLEAR', 'PENDING', 'APPROVED', 'REJECTED'], {
         useNative: true,
         enumName: 'status',
       });
+      t.integer('CohortID')
+        .notNullable()
+        .unsigned()
+        .references('Cohorts.ID')
+        .onUpdate('CASCADE')
+        .onDelete('RESTRICT')
+        .defaultsTo(1);
+      t.dropUnique(['ChildID', 'StoryID']);
+      t.unique(['ChildID', 'StoryID', 'CohortID']);
     })
     .table('Children', (t) => {
       t.integer('CohortID')
@@ -23,7 +32,10 @@ exports.down = function (knex) {
       t.dropColumn('CohortID');
     })
     .table('Submissions', (t) => {
+      t.dropUnique(['ChildID', 'StoryID', 'CohortID']);
+      t.unique(['ChildID', 'StoryID']);
       t.dropColumn('Status');
+      t.dropColumn('CohortID');
     })
     .raw('DROP TYPE status');
 };
