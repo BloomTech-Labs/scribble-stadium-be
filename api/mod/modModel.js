0fb5c022c495a66791eda25ae659604ff2787680
Brandon Ramirez <bran.ramirez.don@gmail.com>
2020-10-08 16:50:13 -0700

    Added JSDocumentation to the model functions for the new submission endpoints.

--- api/mod/modModel.js
+++ api/mod/modModel.js
@@ -1,13 +1,29 @@
 const db = require('../../data/db-config');
 
+/**
+ * Queries the database for a list of all current cohorts
+ * @returns {Promise} returns a promise that resolves to a list of cohort objects
+ */
 const getCohorts = () => {
   return db('Cohorts');
 };
 
+/**
+ * Attempts to add another cohort to the database
+ * @param {Object} cohort a cohort object (or array of cohorts) to be added
+ * @param {number} cohort.StoryID the id of the cohort's curernt story
+ * @return {Promise} returns a promise that resolves to the ID(s) of the new cohort(s)
+ */
 const addCohort = (cohort) => {
   return db('Cohorts').insert(cohort).returning('ID');
 };
 
+/**
+ * Returns a hash table list of all submissions for a given cohort. Response documentation
+ * can be found on the GET /mod/submissions?cohortId={} endpoint.
+ * @param {number} CohortID the id of the desired cohort
+ * @returns {Promise} a promise that resolves to a table of submissions
+ */
 const getSubmissionsByCohort = async (CohortID) => {
   const data = await db('Submissions AS S')
     .where({ CohortID })
@@ -42,8 +58,15 @@ const getSubmissionsByCohort = async (CohortID) => {
   return res;
 };
 
-const moderatePost = (ID, Status) => {
-  return db('Submissions').where({ ID }).update(Status);
+/**
+ * Attempts to update the submission status of a given post.
+ * @param {number} ID the id of the submission to update
+ * @param {Object} changes the desired changes for the given submission
+ * @param {string} changes.Status a string containing one of the following flags: 'APPROVED', 'REJECTED'
+ * @returns {Promise} returns a promise that resolves to a count of updated rows
+ */
+const moderatePost = (ID, changes) => {
+  return db('Submissions').where({ ID }).update(changes);
 };
 
 module.exports = {
