b9bc6aacc796c9ddbc5e252f239678596ac810dd
Brandon Ramirez <bran.ramirez.don@gmail.com>
2020-10-19 16:19:00 -0700

    Added JSDocs to faceoffRetrieval

--- api/game/gameHelpers/faceoffRetrieval.js
+++ api/game/gameHelpers/faceoffRetrieval.js
@@ -1,5 +1,10 @@
 const { dbOps, formatSubForMatchups } = require('../../../lib');
 
+/**
+ * This function queries the database for a list of Submission IDs for a given squad's faceoffs.
+ * @param {Object} conn knex client connection
+ * @param {number} SquadID unique integer squad id
+ */
 const getSubIdsForFaceoffs = (conn, SquadID) => {
   return conn('Faceoffs AS F')
     .join('Squads AS S', 'S.ID', 'F.SquadID')
@@ -7,6 +12,12 @@ const getSubIdsForFaceoffs = (conn, SquadID) => {
     .select('F.*');
 };
 
+/**
+ * This function adds submission data to the faceoffs based on the ids returned from the
+ * getSubIdsForFaceoffs() function above.
+ * @param {Object} conn a knex client object
+ * @param {Object} faceoffs an object containing faceoffs
+ */
 const addSubmissionsToFaceoffs = async (conn, faceoffs) => {
   for (let f in faceoffs) {
     const s1 = await dbOps.getSubByID(conn, faceoffs[f].SubmissionID1);
