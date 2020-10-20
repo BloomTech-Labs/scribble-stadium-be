3121c02cdb1813980dc4ac31ba4285efd8f47439
Brandon Ramirez <bran.ramirez.don@gmail.com>
2020-10-20 15:29:36 -0700

    BIG BUGFIX

--- data/migrations/20201007143802_cohorts.js
+++ data/migrations/20201007143802_cohorts.js
@@ -1,17 +1,13 @@
 exports.up = function (knex) {
-  return knex.schema
-    .createTable('Cohorts', (t) => {
-      t.increments('ID');
-      t.integer('StoryID')
-        .notNullable()
-        .unsigned()
-        .references('Stories.ID')
-        .onUpdate('CASCADE')
-        .onDelete('RESTRICT');
-    })
-    .then(() => {
-      return knex('Cohorts').insert({ StoryID: 1 });
-    });
+  return knex.schema.createTable('Cohorts', (t) => {
+    t.increments('ID');
+    t.integer('StoryID')
+      .notNullable()
+      .unsigned()
+      .references('Stories.ID')
+      .onUpdate('CASCADE')
+      .onDelete('RESTRICT');
+  });
 };
 
 exports.down = function (knex) {
