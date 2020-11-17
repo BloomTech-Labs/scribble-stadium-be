04171441ba4f8830104aeb57e41bd77c9671ca4b
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-11-17 08:53:39 -0800

    UReduces number of children and updates Poinits to total up to 100

--- data/seeds/012_Points.js
+++ data/seeds/012_Points.js
@@ -1,11 +1,11 @@
-const points = [...new Array(16)].map((i, idx) => ({
-  WritingPoints: Math.floor(Math.random() * 100),
-  DrawingPoints: Math.floor(Math.random() * 100),
+const points = [...new Array(8)].map((i, idx) => ({
+  WritingPoints: 30,
+  DrawingPoints: 20,
   MemberID: `${Math.floor((idx + 2) / 2)}`,
-  SubmissionID: `${idx + 1}`
+  SubmissionID: `${idx + 1}`,
 }));
 
-exports.seed = function(knex) {
+exports.seed = function (knex) {
   // Inserts seed entries
   return knex('Points').insert(points);
 };
