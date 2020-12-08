3379cdd1165d0353b95bfd395254bd40e8148abc
ajg7 <ajgebara@gmail.com>
2020-12-08 15:06:37 -0600

    can successfully validate route use based on date

--- api/middleware/dateValidation.js
+++ api/middleware/dateValidation.js
@@ -4,17 +4,23 @@ const currentDayOfTheWeek = currentDate.getDay();
 const readWriteDrawDateValidation = (request, response, next) => {
     if (currentDayOfTheWeek === 1 ||
         currentDayOfTheWeek === 2 ||
-        currentDayOfTheWeek === 3) console.log('It is either Monday, Tuesday, or Wednesday!!!');
+        currentDayOfTheWeek === 3) {
+            console.log('It is either Monday, Tuesday, or Wednesday!!!');
+    } else throw new Error("It is not Monday, Tuesday, or Wednesday")
     next();
 }
 
 const pointShareDateValidation = (request, response, next) => {
-    if (currentDayOfTheWeek === 4) console.log('It is Thursday');
+    if (currentDayOfTheWeek === 4) {
+        console.log('It is Thursday');
+    } else throw new Error("It is not Thrusday")
     next();
 }
 
 const faceOffDateValidation = (request, response, next) => {
-    if (currentDayOfTheWeek === 5) console.log('Friday, Friday');
+    if (currentDayOfTheWeek === 5) {
+        console.log('Friday, Friday');
+    } else throw new Error("It is not Friday")
     next();
 }
 
