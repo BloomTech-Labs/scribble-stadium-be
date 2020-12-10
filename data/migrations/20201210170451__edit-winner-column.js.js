316f7baf4a8af9e6be930cba763eeb9dcc90a9c5
ajg7 <ajgebara@gmail.com>
2020-12-10 17:31:57 -0600

    Added a new winner column with integer type in faceoffs and total_points column in children

---
+++ data/migrations/20201210170451__edit-winner-column.js.js
@@ -0,0 +1,11 @@
+exports.up = function (knex) {
+    return knex.schema.table('Faceoffs', table => {
+        table.dropColumn('Winner')
+    });
+};
+
+exports.down = function (knex) {
+    return knex.schema.table('Faceoffs', table => {
+        table.dropColumn('Winner')
+    });
+};
