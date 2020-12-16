33f85c816a77c58745d1770fe23f01b932a15c11
DeeDowns <dee_downs@outlook.com>
2020-12-15 19:57:21 -0500

    moved variable declarations to top of function to fix undefined issues; re-wrote logic for if conditional; removed console.logs

--- api/middleware/emojiValidation.js
+++ api/middleware/emojiValidation.js
@@ -1,23 +1,21 @@
 const emojiRegex = require('emoji-regex/text')
 
 function emojiValidation(req, res, next) {
-    function isOnlyEmojis(message) {
-      if (!message) return true;
+  const emojiString1 = req.body.subEmojis1
+  const emojiString2 = req.body.subEmojis2
+    
+  function isOnlyEmojis(message) {
+    if (!message) return true;
     
       const noEmojis = message.replace(emojiRegex(), "");
-      console.log("1. noEmojis", noEmojis);
       const noSpace = noEmojis.replace(/[\s\n]/gm, "");
-      console.log("nospace", noSpace);
-    
-      return !noSpace;
-    }
   
-    const emojiString1 = req.body.subEmoji1
-    const emojiString2 = req.body.subEmoji2
+      return !noSpace
+  }
+    
     if ((isOnlyEmojis(emojiString1) && isOnlyEmojis(emojiString2)) && (emojiString1.length <= 6 && emojiString2.length <= 6)) {
       next()
     } else {
-      console.log('hol up')
       res.status(403).json({ message: 'feedback must be a string of no more than 6 emojis'})
     }
 }
