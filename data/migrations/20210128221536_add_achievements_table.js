e8aa35232bc4af1f2512eeffce706757fdd424d6
hernandezm-dev <hernandezm.dev@gmail.com>
2021-01-28 23:17:33 -0500

    added Achievements table and seeds for table

--- data/migrations/20210128221536_add_achievements_table.js
+++ data/migrations/20210128221536_add_achievements_table.js
@@ -1,11 +1,13 @@
 
 exports.up = function(knex) {
     return knex.schema.createTable('Achievements', table=>{
+        table.increments('ID');
         table.string('Name').unsigned();
         table.string('Description').unsigned()
     })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema
+    .dropTableIfExists('Achievements')
 };
