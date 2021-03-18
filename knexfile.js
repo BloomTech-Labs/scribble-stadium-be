4082b2e52f0146552036e42424d102a2b5cbe99b
Sam Munro <sjm.munro@gmail.com>
2021-03-18 18:18:23 -0400

    ref: fixed path for migrations and seeds

--- knexfile.js
+++ knexfile.js
@@ -5,8 +5,8 @@ module.exports = {
   development: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
-    migrations: { directory: '../data/migrations' },
-    seeds: { directory: '../data/seeds' },
+    migrations: { directory: './data/migrations' },
+    seeds: { directory: './data/seeds' },
     pool: {
       min: 2,
       max: 10,
@@ -16,30 +16,30 @@ module.exports = {
   test: {
     client: 'pg',
     connection: process.env.TEST_DATABASE_URL,
-    migrations: { directory: '../data/migrations' },
-    seeds: { directory: '../data/seeds' },
+    migrations: { directory: './data/migrations' },
+    seeds: { directory: './data/seeds' },
     pool: { min: 0, max: 5 },
   },
 
   ci: {
     client: 'pg',
     connection: process.env.CI_DATABASE_URL,
-    migrations: { directory: '../data/migrations' },
-    seeds: { directory: '../data/seeds' },
+    migrations: { directory: './data/migrations' },
+    seeds: { directory: './data/seeds' },
     pool: { min: 0, max: 5 },
   },
 
   production: {
     client: 'pg',
     connection: process.env.DATABASE,
-    migrations: { directory: '../data/migrations' },
-    seeds: { directory: '../data/seeds' },
+    migrations: { directory: './data/migrations' },
+    seeds: { directory: './data/seeds' },
   },
 
   staging: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
-    migrations: { directory: '../data/migrations' },
-    seeds: { directory: '../data/seeds' },
+    migrations: { directory: './data/migrations' },
+    seeds: { directory: './data/seeds' },
   },
 };
