5ffab99f73bd037a758c4f9fc85566c5e064b8e6
Ryan Hamblin <8798259+ryan-hamblin@users.noreply.github.com>
2021-03-22 12:29:36 -0600

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -32,6 +32,9 @@ module.exports = {
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    },
     migrations: { directory: './data/migrations' },
     seeds: { directory: './data/seeds' },
   },
