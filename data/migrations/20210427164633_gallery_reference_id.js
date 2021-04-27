ba3bc1d3f2b20cee8d4e5f32515e1f062611287a
Jacob Ekegren <JacobEkegren@MacBook-Pro.local>
2021-04-27 16:55:37 -0700

    added reference id for gallary table to children table

---
+++ data/migrations/20210427164633_gallery_reference_id.js
@@ -0,0 +1,10 @@
+
+exports.up = function(knex) {
+  return knex.schema.table('Gallary', table => {
+      table.integer("children_id").unsigned().notNull().references("id").inTable("Children").onUpdate("CASCADE").onDelete("CASCADE")
+  })
+};
+
+exports.down = function(knex) {
+  return knex.dropColumn("children_id")
+};
