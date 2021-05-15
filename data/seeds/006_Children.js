a1d93dab070e3ce73f504e77d3a75cb16ef9f2af
Tucker <leachtucker@gmail.com>
2021-05-14 17:36:32 -0700

    Modfies seed data to comply with schema

--- data/seeds/006_Children.js
+++ data/seeds/006_Children.js
@@ -16,7 +16,9 @@ const children1 = [...new Array(8)].map((i, idx) => ({
   // Attempting to have wins & losses combine to equal 4 (1 less than the cohort's StoryID, which should be how many weeks/rounds they've finished). Check table to see if it works.
   Wins: `${(idx) % 5}`,        // 0,1,2,3,4,0
   Losses: `${(99 - idx) % 5}`, // 4,3,2,1,0,4
-  Total_Points: `${faker.random.number({ min: 0, max: 400 }) * 4}`
+  Total_Points: `${faker.random.number({ min: 0, max: 400 }) * 4}`,
+  Email: `${faker.internet.email()}`,
+  CharacterName: `${faker.hacker.adjective} ${faker.animal}`
 }));
 
 const children2 = [...new Array(8)].map((i, idx) => ({
@@ -33,7 +35,9 @@ const children2 = [...new Array(8)].map((i, idx) => ({
   // Attempting to have wins & losses combine to equal 3 (1 less than the cohort's StoryID, which should be how many weeks/rounds they've finished). Check table to see if it works.
   Wins: `${(idx) % 4}`,        // 0,1,2,3,4,0
   Losses: `${(99 - idx) % 4}`, // 4,3,2,1,0,4
-  Total_Points: `${faker.random.number({ min: 0, max: 400 }) * 3}`
+  Total_Points: `${faker.random.number({ min: 0, max: 400 }) * 3}`,
+  Email: `${faker.internet.email()}`,
+  CharacterName: `${faker.hacker.adjective} ${faker.animal}`
 }));
 
 const children3 = [...new Array(8)].map((i, idx) => ({
@@ -50,7 +54,9 @@ const children3 = [...new Array(8)].map((i, idx) => ({
   // Attempting to have wins & losses combine to equal 1 (1 less than the cohort's StoryID, which should be how many weeks/rounds they've finished). Check table to see if it works.
   Wins: `${(idx) % 2}`,        // 0,1,2,3,4,0
   Losses: `${(99 - idx) % 2}`, // 4,3,2,1,0,4
-  Total_Points: `${faker.random.number({ min: 0, max: 400 }) * 1}`
+  Total_Points: `${faker.random.number({ min: 0, max: 400 }) * 1}`,
+  Email: `${faker.internet.email()}`,
+  CharacterName: `${faker.hacker.adjective} ${faker.animal}`
 }));
 
 const children4 = [...new Array(8)].map((i, idx) => ({
@@ -65,7 +71,9 @@ const children4 = [...new Array(8)].map((i, idx) => ({
   // Wins & losses combine to equal 0, bc it's cohort 4's first time playing.
   Wins: `0`,
   Losses: `0`,
-  Total_Points: 0
+  Total_Points: 0,
+  Email: `${faker.internet.email()}`,
+  CharacterName: `${faker.hacker.adjective} ${faker.animal}`
 }));
 
 const children = children1.concat(children2, children3, children4)
