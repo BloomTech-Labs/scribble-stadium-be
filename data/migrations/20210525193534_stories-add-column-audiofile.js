296df7f6ad8633b3361c0c747708f4653da6ecfa
KVlearn <kavya.mdu@gmail.com>
2021-05-25 20:42:22 -0700

    eslint format error fixed

--- data/migrations/20210525193534_stories-add-column-audiofile.js
+++ data/migrations/20210525193534_stories-add-column-audiofile.js
@@ -1,12 +1,11 @@
 exports.up = function (knex) {
-    return knex.schema.table('Stories', (table) => {
-      table.string('Audiofile').notNullable();
-    });
-  };
-  
-  exports.down = function (knex) {
-    return knex.schema.table('Stories', (table) => {
-      table.dropColumn('Audiofile');
-    });
-  };
-  
+  return knex.schema.table('Stories', (table) => {
+    table.string('Audiofile').notNullable();
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.table('Stories', (table) => {
+    table.dropColumn('Audiofile');
+  });
+};
