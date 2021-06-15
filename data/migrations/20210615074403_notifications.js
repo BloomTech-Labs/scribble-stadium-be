9d86f310effce1584f57af3c11a43ebcd2daee5c
Kevin St.Onge <kevinstonge@gmail.com>
2021-06-15 10:31:24 -0400

    added primary key to notification tables, also added LinksTo row so that notifications can be associated with specific resources and be clickable on the front-end

--- data/migrations/20210615074403_notifications.js
+++ data/migrations/20210615074403_notifications.js
@@ -1,6 +1,7 @@
 exports.up = function (knex) {
   return knex.schema
     .createTable('Parent-Notifications', (parentNotifications) => {
+      parentNotifications.increments('ID');
       parentNotifications.integer('ParentID')
         .notNullable()
         .references('Parents.ID')
@@ -8,10 +9,15 @@ exports.up = function (knex) {
         .onDelete('RESTRICT');
       parentNotifications.string('Notification').notNullable();
       parentNotifications.boolean('Read').notNullable().defaultTo(false);
+      //The notification "Type" will be used to style the notification on the front-end
+      //possible/example Types: information, warning, goodnews
       parentNotifications.string('Type').notNullable().defaultTo('information');
+      //"LinksTo" could be a string indicating where the user will be directed when they click on the notification
+      parentNotifications.string('LinksTo').notNullable();
       parentNotifications.timestamp('Date').notNullable().defaultTo(knex.fn.now());
     })
     .createTable('Child-Notifications', (childNotifications) => {
+      childNotifications.increments('ID');
       childNotifications.integer('ChildID')
         .notNullable()
         .references('Children.ID')
@@ -19,7 +25,8 @@ exports.up = function (knex) {
         .onDelete('RESTRICT');
       childNotifications.string('Notification').notNullable();
       childNotifications.boolean('Read').notNullable().defaultTo(false);
-      childNotifications.string('Type').notNullable().defaultTo('information')
+      childNotifications.string('Type').notNullable().defaultTo('information');
+      childNotifications.string('LinksTo').notNullable();
       childNotifications.timestamp('Date').notNullable().defaultTo(knex.fn.now());
     });
 };
