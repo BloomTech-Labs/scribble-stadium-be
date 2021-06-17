f55ecd5214b5fc8038cabc9baf4b0b9be07b9219
Kevin St.Onge <kevinstonge@gmail.com>
2021-06-16 20:31:09 -0400

    refactored migration file to use bridge tables to reduce load on database

--- data/migrations/20210615074403_notifications.js
+++ data/migrations/20210615074403_notifications.js
@@ -1,44 +1,52 @@
 exports.up = function (knex) {
   return knex.schema
-    .createTable('Parent-Notifications', (parentNotifications) => {
-      parentNotifications.increments('ID');
-      parentNotifications.integer('ParentID')
+    .createTable('Notifications', (notifications) => {
+      notifications.increments('ID');
+      notifications.string('Text').notNullable();
+      notifications.boolean('Read').notNullable().defaultTo(false);
+      //The notification "Type" will be used to style the notification on the front-end
+      //possible/example Types: information, warning, goodnews
+      notifications.string('Type').notNullable().defaultTo('information');
+      //"LinksTo" could be a string indicating where the user will be directed when they click on the notification
+      notifications.string('LinksTo').notNullable();
+      notifications.timestamp('Date').notNullable().defaultTo(knex.fn.now());
+      notifications.timestamp('DueDate');
+    })
+    .createTable('Children-Notifications', (childrenNotifications) => {
+      childrenNotifications.integer('ChildID')
+        .notNullable()
+        .references('Children.ID')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+      childrenNotifications.integer('NotificationID')
+        .notNullable()
+        .references('Notifications.ID')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+      childrenNotifications.boolean('Read').notNullable().defaultTo(false);
+    })
+    .createTable('Parents-Notifications', (parentsNotifications) => {
+      parentsNotifications.integer('ParentID')
         .notNullable()
         .references('Parents.ID')
         .onUpdate('CASCADE')
-        .onDelete('RESTRICT');
-      parentNotifications.string('Notification').notNullable();
-      parentNotifications.boolean('Read').notNullable().defaultTo(false);
-      //The notification "Type" will be used to style the notification on the front-end
-      //possible/example Types: information, warning, goodnews
-      parentNotifications.string('Type').notNullable().defaultTo('information');
-      //"LinksTo" could be a string indicating where the user will be directed when they click on the notification
-      parentNotifications.string('LinksTo').notNullable();
-      parentNotifications.integer('ChildID')
+        .onDelete('CASCADE');
+      parentsNotifications.integer('ChildID')
         .references('Children.ID')
         .onUpdate('CASCADE')
-        .onDelete('RESTRICT');
-      parentNotifications.timestamp('Date').notNullable().defaultTo(knex.fn.now());
-      parentNotifications.timestamp('DueDate');
-    })
-    .createTable('Child-Notifications', (childNotifications) => {
-      childNotifications.increments('ID');
-      childNotifications.integer('ChildID')
+        .onDelete('CASCADE');
+      parentsNotifications.integer('NotificationID')
         .notNullable()
-        .references('Children.ID')
+        .references('Notifications.ID')
         .onUpdate('CASCADE')
-        .onDelete('RESTRICT');
-      childNotifications.string('Notification').notNullable();
-      childNotifications.boolean('Read').notNullable().defaultTo(false);
-      childNotifications.string('Type').notNullable().defaultTo('information');
-      childNotifications.string('LinksTo').notNullable();
-      childNotifications.timestamp('Date').notNullable().defaultTo(knex.fn.now());
-      childNotifications.timestamp('DueDate');
+        .onDelete('CASCADE');
+      parentsNotifications.boolean('Read').notNullable().defaultTo(false);
     });
 };
 
 exports.down = function (knex) {
   return knex.schema
-    .dropTableIfExists('Parent-Notifications')
-    .dropTableIfExists('Child-Notifications');
+    .dropTableIfExists('Notifications')
+    .dropTableIfExists('Children-Notifications')
+    .dropTableIfExists('Parents-Notifications');
 };
