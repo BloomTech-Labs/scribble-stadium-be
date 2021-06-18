0111190110f35be7abcbb662a21b07788670f032
Alex Magana <46731475+jamcoding@users.noreply.github.com>
2021-06-17 19:35:10 -0500

    fix lint errors

--- data/migrations/20210616205309_gallery_submissions.js
+++ data/migrations/20210616205309_gallery_submissions.js
@@ -1,24 +1,23 @@
-
-exports.up = function(knex) {
-  return knex.schema
-    .createTable('Gallery_Submissions', (table) => {
-        table.increments('children_submissions_id');
-        table.timestamps(true, false);
-        table.integer('sprint')
-          .notNullable();
-        table.interger('children_id')
-          .references('children_id')
-          .inTable('Gallary')
-          .onUpdate('CASCADE')
-          .onDelete('CASCADE');
-        table.interger('submissions_id')
-          .references('ID')
-          .inTable('Submissions')
-          .onUpdate('CASCADE')
-          .onDelete('CASCADE');
-    })
+exports.up = function (knex) {
+  return knex.schema.createTable('Gallery_Submissions', (table) => {
+    table.increments('children_submissions_id');
+    table.timestamps(true, false);
+    table.integer('sprint').notNullable();
+    table
+      .interger('children_id')
+      .references('children_id')
+      .inTable('Gallary')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+    table
+      .interger('submissions_id')
+      .references('ID')
+      .inTable('Submissions')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+  });
 };
-  
-exports.down = function(knex) {
+
+exports.down = function (knex) {
   return knex.schema.dropTableIfExists('Gallery_Submissions');
 };
