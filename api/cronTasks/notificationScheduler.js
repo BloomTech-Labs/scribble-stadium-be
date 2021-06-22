eba7e571734e37cfd47dd6f8a4840dfda91c77ab
Kevin St.Onge <kevinstonge@gmail.com>
2021-06-21 20:02:00 -0400

    filled in all of the main calendar tasks (days 1-6), code could be made much more DRY

--- api/cronTasks/notificationScheduler.js
+++ api/cronTasks/notificationScheduler.js
@@ -2,7 +2,8 @@ const cron = require('node-cron');
 const cronNotificationTasks = require('./cronNotificationTasks');
 // Create notification for new story to read:
 
-cron.schedule('0 9 * * 5', async () => {
+//day one - READ - (Saturday)
+cron.schedule('0 9 * * 6', async () => {
     try {
         const notificationID = await cronNotificationTasks.createNotification({
             "Text": "You have a new story to read!",
@@ -13,7 +14,7 @@ cron.schedule('0 9 * * 5', async () => {
             //error handling?
         }
         else {
-            console.log('error in notificationScheduler.js line 19');
+            console.log('error in notificationScheduler.js -READ-');
         }
     }
     catch (err) {
@@ -22,6 +23,7 @@ cron.schedule('0 9 * * 5', async () => {
     }
 })
 
+//day two - DRAW - (Monday)
 cron.schedule('0 9 * * 1', async () => {
     try {
         const notificationID = await cronNotificationTasks.createNotification({
@@ -32,7 +34,70 @@ cron.schedule('0 9 * * 1', async () => {
             await cronNotificationTasks.populateBridgeTable('Children', notificationID[0]);
         }
         else {
-            console.log('error in notificationScheduler, line 35');
+            console.log('error in notificationScheduler, -DRAW-');
+        }
+    }
+    catch (err) {
+        console.log(err);
+        throw err;
+    }
+});
+
+//day three - WRITE - (Tuesday)
+cron.schedule('0 9 * * 2', async () => {
+    try {
+        const notificationID = await cronNotificationTasks.createNotification({
+            "Text": "Write a story based on the story you read",
+            "LinksTo": "[placeholder for write link]",
+        });
+        if (notificationID[0]) {
+            await cronNotificationTasks.populateBridgeTable('Children', notificationID[0]);
+        }
+        else {
+            console.log('error in notificationScheduler, -WRITE-');
+        }
+    }
+    catch (err) {
+        console.log(err);
+        throw err;
+    }
+})
+
+//day four - notifications for staff - (Wednesday)
+//~~code here~~
+
+//day five - POINTSHARE/PAIRINGS - (Thursday)
+cron.schedule('0 9 * * 4', async () => {
+    try {
+        const notificationID = await cronNotificationTasks.createNotification({
+            "Text": "See who you are paired with, use your points to give your team the best chance to win!",
+            "LinksTo": "[placeholder for pointshare link]",
+        });
+        if (notificationID[0]) {
+            await cronNotificationTasks.populateBridgeTable('Children', notificationID[0]);
+        }
+        else {
+            console.log('error in notificationScheduler, -POINTSHARE-');
+        }
+    }
+    catch (err) {
+        console.log(err);
+        throw err;
+    }
+})
+
+//day six - VOTE - (Friday)
+cron.schedule('0 9 * * 5', async () => {
+    try {
+        const notificationID = await cronNotificationTasks.createNotification({
+            "Text": "Vote for the best writing and drawing!",
+            "LinksTo": "[placeholder for voting link]",
+        });
+        if (notificationID[0]) {
+            await cronNotificationTasks.populateBridgeTable('Children', notificationID[0]);
+        }
+        else {
+            console.log('error in notificationScheduler, -VOTE-');
         }
     }
     catch (err) {
