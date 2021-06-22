0981e6f33147299df5fb16c4edc6990b3a3d2404
Kevin St.Onge <kevinstonge@gmail.com>
2021-06-21 22:02:35 -0400

    attempting to fix linting errors

--- api/cronTasks/cronNotificationTasks.js
+++ api/cronTasks/cronNotificationTasks.js
@@ -1,31 +1,23 @@
 const db = require('../../data/db-config');
 const createNotification = async (notificationObject) => {
-  try {
     if (notificationObject["Text"]) {
       return await db('Notifications')
         .insert({
           'Text': notificationObject['Text'],
           'Type': notificationObject['Type'] || 'information',
           'LinksTo': notificationObject['LinksTo'] || '',
-          'Date': notificationObject['Date'] || new Date,
+          'Date': notificationObject['Date'] || new Date(),
           'DueDate': notificationObject['DueDate'] || null
         }).returning('ID');
     }
     console.log('error in cronNotificationTasks.js line 15');
     return false;
-  }
-  catch (err) {
-    throw err;
-    console.log(err);
-  }
 };
-
 const populateBridgeTable = async (userTable, notificationID) => {
-  try {
-    const userIDString = () => {
-      if (userTable === "Children") { return "ChildID" }
-      if (userTable === "Parents") { return "ParentID" }
-      if (userTable === "Staff") { return "StaffID" }
+    const userIDStrings = {
+      'Children': 'ChildID',
+      'Parents': 'ParentID',
+      'Staff': 'StaffID',
     }
     const userIDs = await db(userTable).pluck("ID");
     const notificationExists = await db('Notifications').where({ ID: notificationID });
@@ -33,8 +25,8 @@ const populateBridgeTable = async (userTable, notificationID) => {
       const bridgeTableArray = userIDs.map(id => {
         if (id > 0) {
           return ({
-            [userIDString()]: parseInt(id),
-            "NotificationID": parseInt(notificationID)
+            [userIDStrings[userTable]]: parseInt(id),
+            'NotificationID': parseInt(notificationID)
           });
         }
       });
@@ -44,11 +36,6 @@ const populateBridgeTable = async (userTable, notificationID) => {
       }
     }
     return false;
-  }
-  catch (err) {
-    console.log(err);
-    throw err;
-  }
 }
 
 module.exports = { createNotification, populateBridgeTable }
