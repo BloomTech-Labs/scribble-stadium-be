03a6038f89e5c9777f475feb3e8e859a24d94a21
Shirley Lee <synchrotronflux@yahoo.com>
2021-07-02 12:23:32 -0400

    response body takes in child user & parent validation, currentDayOfWeek

--- api/cronTasks/notificationRouter.js
+++ api/cronTasks/notificationRouter.js
@@ -1,57 +1,73 @@
-/*Handle routes and HTTP request methods within an Express project. 
-Routes handle user navigation to various URLs throughout your application. 
-HTTP communicates and facilitates data from your Express server to the web browser. HTTP request methods GET, POST, PUT, and DELETE to ... data.*/
+
 const router = require('express').Router();
-const CronTasks = require('./cronTasks');
+
+
+const CronTasks = require('./cronTasks');  //notifications
+const childInfo = require('../child/childModel'); // getComplexityList    getById(child)   getDay()dateValidation/Router
+
+// authRequired = require('../middleware/authRequired')
+const {authRequired, childValidation, childUpdateValidation,} = require('../child/middleware');
+// const {parentValidation, parentUpdateValidation,};  // const restrict = require("../users/userValidation");  does not exist
+
+const currentDayOfTheWeek = require('../middleware/dateValidation')  // readWriteDrawDateValidation, pointsShareDateValidation, independentVotingDateValidation
+// ..middleware/submissionsValidation
 
 
 
-// Step Enabled Notification    (post submission)
-router.get('/squad', (req, res) =>{
-try{
-  crudOperationsManager.getAll(res, leaderboard.getTotalPointsToChildren, 'Child'),
-    res.status(200).json({message: `Congratulations ${user.username}! You have unlocked ${`assignmentsAdded`} acitivity and have ${days} to complete it`,
-});
-} catch (err) {
-  console.log(err)
-  next(err);
-}
-})
+// Regular update    message: `Welcome ${user.username}, You have ${days} to complete your ${assignment} 
+router.get('/:id', async (req, res, next) => {    //child/:id/dashboard/:id
+    try{
+        const getClass = await childInfo.getClasses(req.params.id);
+        if(!getClass){
+            return res.status(400).json({
+                message: "Invalid ID"
+            })
+        }
 
-// Login Notification -Next Task  (read, write, draw, pointshare, vote)
-router.post("/login", async (req, res, next) => {
-try {
-  // res.status(200).json(notifications);
-  res.json({
-    message: `Welcome ${user.username}, You have ${days} to complete your ${assignment} activity. Good Luck!`,
-  });
-} catch (err) {
-  console.log(err)
-  next(err);
-}
-})
+        return res.status(200).json(getClass);
+    } catch(error){
+        next(error)
+    }
+}) 
 
 
-// Login Notification -Not Completed
-router.post("/login", async (req, res, next) => {
-try {
-  res.status(200).json({message: `Welcome ${user.username}, You have not completed your ${assignment} assignment. You have ${days} to complete this to unlock the next adventure! See you there!`,
-});
-} catch (err) {
-  console.log(err)
-  next(err);
-}
-})
+//  If incomplete, reads different message. has different color
+//     message: `Welcome ${user.username}, You have not completed your ${assignment} assignment. You have ${days} to complete this to unlock the next adventure! See you there!`,
+// });
+router.get('/:id', async (req, res, next) => {    //child/:id/dashboard/:id
+  try{
+      const getClass = await childInfo.getClasses(req.params.id);
+      if(!getClass){
+          return res.status(400).json({
+              message: "Invalid ID"
+          })
+      }
 
-
-/* 
-1  the response body should have `notification/message` and `token`:
-  {
-    "message": "Welcome, <Username>, You have <# of days> to complete your <assignment>",
-    "token": "eyJhbGciOiJIUzI ... ETC ... vUPjZYDSa46Nwz8"
-      
+      return res.status(200).json(getClass);
+  } catch(error){
+      next(error)
   }
-*/  
+})
+
+
+
+// // Step Enabled Notification    (post submission)
+// router.get('/squad', (req, res) =>{
+// try{
+//   crudOperationsManager.getAll(res, leaderboard.getTotalPointsToChildren, 'Child'),
+//     res.status(200).json({message: `Congratulations ${user.username}! You have unlocked ${`assignmentsAdded`} acitivity and have ${days} to complete it`,
+// });
+// } catch (err) {
+//   console.log(err)
+//   next(err);
+// }
+// })
+
+
+//  const childValidation = (...r) => {
+//   checkInit(...r, fields, 'Child');
+// };
+
 
 
 
