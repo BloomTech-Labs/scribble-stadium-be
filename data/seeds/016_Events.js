f6062a504b7037cfc95654824d87afe4a3bfb167
Kevin St.Onge <kevinstonge@gmail.com>
2021-07-04 11:04:12 -0400

    .

--- data/seeds/016_Events.js
+++ data/seeds/016_Events.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('Events').del()
     .then(function () {
       // Inserts seed entries
       return knex('Events').insert([
