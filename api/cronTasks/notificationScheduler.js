f6e62c0e8ca814507f13bc76c93575006793babf
Kevin St.Onge <kevinstonge@gmail.com>
2021-07-07 20:57:21 -0400

    created a function that converts all cron-strings into arrays of numbers (to be used to sort cron strings to determine which ones need to run in development or testing environments)

--- api/cronTasks/notificationScheduler.js
+++ api/cronTasks/notificationScheduler.js
@@ -2,6 +2,16 @@ const cron = require('node-cron');
 const eventTasks = require('./eventTasks.js');
 const db = require('../../data/db-config.js');
 
+if (process.env.NODE_ENV === "test" || process.env.NODE_ENV === "development") {
+  db('Events').then((events) => {
+    const firstEventCronString = "0 9 * * Friday";
+    const mockedCurrentCronString = "0 9 * * Tuesday";
+    //will need to sort event openings and closings sequentially relative to firstEventConString, then filter out those not inclusively between firstEventCronString and mockedCurrentCronString
+    //need a function to parse cron strings 
+    
+  })
+}
+
 db('Events').then((events) => {
   events.forEach((event) => {
     if (event.Open) {
