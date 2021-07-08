76eff40b266a4f2b515e52463539411dd1538530
Kevin St.Onge <kevinstonge@gmail.com>
2021-07-07 20:32:38 -0400

    Feature/event tables seeds and scheduler (#46)
    * create tables related to events in new migration file
    
    * added Events seeds for basic events
    
    * working on refactoring notification table scheduled population and including scheduled event table updates
    
    * .
    
    * event tables, seeds, and scheduler created/updated
    
    * deleted old file
    
    * made some progress with testing, will need to write conditional statements in core functions to be able to properly develop further since the code depends on what day of the week it is and we do not want to have to wait until arbitrary days of the week to test features properly
    
    * added staff table so that existing scheduled events can run
    
    * fix linting errors
    
    * fixed staff review link

--- __tests__/routes/cron-notification-tests.js
+++ __tests__/routes/cron-notification-tests.js
@@ -1,24 +1,18 @@
 const db = require('../../data/db-config');
 const cron = require('node-cron');
-jest.mock('node-cron', () => {
-  return {
-    schedule: jest.fn(),
-  };
+cron.schedule = jest.fn().mockImplementation(async (frequency, callback) => {
+  return await callback();
 });
 beforeAll(async () => {
-  return await db('Notifications').del();
+  await db('Children-Events').del();
+  await db('Notifications').del();
+  await require('../../api/cronTasks/notificationScheduler.js');
+  return;
 });
 describe('test does something', () => {
-  it('should do something', () => {
-    cron.schedule.mockImplementation(async (frequency, callback) => {
-      await callback();
-    });
-    require('../../api/cronTasks/notificationScheduler');
-    expect(cron.schedule).toBeCalledWith('0 9 * * 5', expect.any(Function));
-  });
   it('should populate bridge table', async () => {
     await new Promise((r) => setTimeout(r, 1000)); //with all the async and await stuff, not sure why this is needed, but for now it gets this test to pass!
-    const data = await db('Children-Notifications');
+    const data = await db('Children-Events');
     expect(data.length).toBeGreaterThan(10);
   });
 });
--- api/cronTasks/notificationScheduler.js
+++ api/cronTasks/notificationScheduler.js
@@ -1,114 +1,34 @@
 const cron = require('node-cron');
-const cronNotificationTasks = require('./cronNotificationTasks');
-// Create notification for new story to read:
+const eventTasks = require('./eventTasks.js');
+const db = require('../../data/db-config.js');
 
-//day one - READ - (Saturday)
-cron.schedule('0 9 * * 6', async () => {
-  try {
-    const notificationID = await cronNotificationTasks.createNotification({
-      Text: 'You have a new story to read!',
-      LinksTo: '/child/story',
-    });
-    if (notificationID[0]) {
-      await cronNotificationTasks.populateBridgeTable(
-        'Children',
-        notificationID[0]
-      );
-    } else {
-      console.log('error in notificationScheduler.js -READ-');
+db('Events').then((events) => {
+  events.forEach((event) => {
+    if (event.Open) {
+      if (cron.validate(event.Open)) {
+        cron.schedule(`'${event.Open}'`, async () => {
+          if (typeof eventTasks[event.Name].open === 'function') {
+            eventTasks[event.Name].open(event);
+          }
+        });
+      } else {
+        console.log(
+          `invalid node-cron string provided for event: ${event.Name}`
+        );
+      }
     }
-  } catch (err) {
-    console.log(err);
-    throw err;
-  }
-});
-
-//day two - DRAW - (Monday)
-cron.schedule('0 9 * * 1', async () => {
-  try {
-    const notificationID = await cronNotificationTasks.createNotification({
-      Text: 'Draw a picture based on the story you read!',
-      LinksTo: '/child/drawing-sub',
-    });
-    if (notificationID[0]) {
-      await cronNotificationTasks.populateBridgeTable(
-        'Children',
-        notificationID[0]
-      );
-    } else {
-      console.log('error in notificationScheduler, -DRAW-');
+    if (event.Close) {
+      if (cron.validate(event.Close)) {
+        cron.schedule(event.Close, async () => {
+          if (typeof eventTasks[event.Name].close === 'function') {
+            eventTasks[event.Name].close(event);
+          }
+        });
+      } else {
+        console.log(
+          `invalid node-cron string provided for event: ${event.Name}`
+        );
+      }
     }
-  } catch (err) {
-    console.log(err);
-    throw err;
-  }
+  });
 });
-
-//day three - WRITE - (Tuesday)
-cron.schedule('0 9 * * 2', async () => {
-  try {
-    const notificationID = await cronNotificationTasks.createNotification({
-      Text: 'Write a story based on the story you read',
-      LinksTo: '/child/writing-sub',
-    });
-    if (notificationID[0]) {
-      await cronNotificationTasks.populateBridgeTable(
-        'Children',
-        notificationID[0]
-      );
-    } else {
-      console.log('error in notificationScheduler, -WRITE-');
-    }
-  } catch (err) {
-    console.log(err);
-    throw err;
-  }
-});
-
-//day four - notifications for staff - (Wednesday)
-//~~code here~~
-
-//day five - POINTSHARE/PAIRINGS - (Thursday)
-cron.schedule('0 9 * * 4', async () => {
-  try {
-    const notificationID = await cronNotificationTasks.createNotification({
-      Text:
-        'See who you are paired with, use your points to give your team the best chance to win!',
-      LinksTo: '/child/point-share',
-    });
-    if (notificationID[0]) {
-      await cronNotificationTasks.populateBridgeTable(
-        'Children',
-        notificationID[0]
-      );
-    } else {
-      console.log('error in notificationScheduler, -POINTSHARE-');
-    }
-  } catch (err) {
-    console.log(err);
-    throw err;
-  }
-});
-
-//day six - VOTE - (Friday)
-cron.schedule('0 9 * * 5', async () => {
-  try {
-    const notificationID = await cronNotificationTasks.createNotification({
-      Text: 'Vote for the best writing and drawing!',
-      LinksTo: '/child/match-up', /// possibly /child/match-up/squad-vote
-    });
-    if (notificationID[0]) {
-      await cronNotificationTasks.populateBridgeTable(
-        'Children',
-        notificationID[0]
-      );
-    } else {
-      console.log('error in notificationScheduler, -VOTE-');
-    }
-  } catch (err) {
-    console.log(err);
-    throw err;
-  }
-});
-// get routes: https://github.com/Lambda-School-Labs/story-squad-fe/blob/main/src/index.js
-//questions: error handling, how to test specific calls to mocked function from same file
