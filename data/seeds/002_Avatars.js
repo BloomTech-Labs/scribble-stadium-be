4098c6bfa093fd7df5b88fa6394eb85015e4fd1e
Trevor Martin <trevor.j.martin@gmail.com>
2021-07-23 00:53:13 -0400

    linted

--- data/seeds/002_Avatars.js
+++ data/seeds/002_Avatars.js
@@ -17,52 +17,68 @@ exports.seed = function (knex) {
     { AvatarURL: 'https://scribble-stadium.s3.amazonaws.com/avatars/15.svg' },
     { AvatarURL: 'https://scribble-stadium.s3.amazonaws.com/avatars/16.svg' },
     {
-      AvatarURL: 'https://scribble-stadium.s3.amazonaws.com/avatars/Green01.svg',
+      AvatarURL:
+        'https://scribble-stadium.s3.amazonaws.com/avatars/Green01.svg',
     },
     {
-      AvatarURL: 'https://scribble-stadium.s3.amazonaws.com/avatars/Green02.svg',
+      AvatarURL:
+        'https://scribble-stadium.s3.amazonaws.com/avatars/Green02.svg',
     },
     {
-      AvatarURL: 'https://scribble-stadium.s3.amazonaws.com/avatars/Green03.svg',
+      AvatarURL:
+        'https://scribble-stadium.s3.amazonaws.com/avatars/Green03.svg',
     },
     {
-      AvatarURL: 'https://scribble-stadium.s3.amazonaws.com/avatars/Green04.svg',
+      AvatarURL:
+        'https://scribble-stadium.s3.amazonaws.com/avatars/Green04.svg',
     },
     {
-      AvatarURL: 'https://scribble-stadium.s3.amazonaws.com/avatars/Green05.svg',
+      AvatarURL:
+        'https://scribble-stadium.s3.amazonaws.com/avatars/Green05.svg',
     },
     {
-      AvatarURL: 'https://scribble-stadium.s3.amazonaws.com/avatars/Green06.svg',
+      AvatarURL:
+        'https://scribble-stadium.s3.amazonaws.com/avatars/Green06.svg',
     },
     {
-      AvatarURL: 'https://scribble-stadium.s3.amazonaws.com/avatars/Green07.svg',
+      AvatarURL:
+        'https://scribble-stadium.s3.amazonaws.com/avatars/Green07.svg',
     },
     {
-      AvatarURL: 'https://scribble-stadium.s3.amazonaws.com/avatars/Green08.svg',
+      AvatarURL:
+        'https://scribble-stadium.s3.amazonaws.com/avatars/Green08.svg',
     },
     {
-      AvatarURL: 'https://scribble-stadium.s3.amazonaws.com/avatars/Green09.svg',
+      AvatarURL:
+        'https://scribble-stadium.s3.amazonaws.com/avatars/Green09.svg',
     },
     {
-      AvatarURL: 'https://scribble-stadium.s3.amazonaws.com/avatars/Green10.svg',
+      AvatarURL:
+        'https://scribble-stadium.s3.amazonaws.com/avatars/Green10.svg',
     },
     {
-      AvatarURL: 'https://scribble-stadium.s3.amazonaws.com/avatars/Green11.svg',
+      AvatarURL:
+        'https://scribble-stadium.s3.amazonaws.com/avatars/Green11.svg',
     },
     {
-      AvatarURL: 'https://scribble-stadium.s3.amazonaws.com/avatars/Green12.svg',
+      AvatarURL:
+        'https://scribble-stadium.s3.amazonaws.com/avatars/Green12.svg',
     },
     {
-      AvatarURL: 'https://scribble-stadium.s3.amazonaws.com/avatars/Green13.svg',
+      AvatarURL:
+        'https://scribble-stadium.s3.amazonaws.com/avatars/Green13.svg',
     },
     {
-      AvatarURL: 'https://scribble-stadium.s3.amazonaws.com/avatars/Green14.svg',
+      AvatarURL:
+        'https://scribble-stadium.s3.amazonaws.com/avatars/Green14.svg',
     },
     {
-      AvatarURL: 'https://scribble-stadium.s3.amazonaws.com/avatars/Green15.svg',
+      AvatarURL:
+        'https://scribble-stadium.s3.amazonaws.com/avatars/Green15.svg',
     },
     {
-      AvatarURL: 'https://scribble-stadium.s3.amazonaws.com/avatars/Green16.svg',
+      AvatarURL:
+        'https://scribble-stadium.s3.amazonaws.com/avatars/Green16.svg',
     },
   ]);
 };
