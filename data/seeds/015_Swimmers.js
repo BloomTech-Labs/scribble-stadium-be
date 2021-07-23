19e7dc8e640b6e585e3f9f9ceffc90bb73adb404
Trevor Martin <trevor.j.martin@gmail.com>
2021-07-23 00:51:33 -0400

    +new s3 bucket

--- data/seeds/015_Swimmers.js
+++ data/seeds/015_Swimmers.js
@@ -2,67 +2,67 @@ exports.seed = function (knex) {
   return knex('Avatars').insert([
     {
       AvatarURL:
-        'https://storysquad-main.s3.amazonaws.com/avatars/Swimmer01.svg',
+        'https://scribble-stadium.s3.amazonaws.com/avatars/Swimmer01.svg',
     },
     {
       AvatarURL:
-        'https://storysquad-main.s3.amazonaws.com/avatars/Swimmer02.svg',
+        'https://scribble-stadium.s3.amazonaws.com/avatars/Swimmer02.svg',
     },
     {
       AvatarURL:
-        'https://storysquad-main.s3.amazonaws.com/avatars/Swimmer03.svg',
+        'https://scribble-stadium.s3.amazonaws.com/avatars/Swimmer03.svg',
     },
     {
       AvatarURL:
-        'https://storysquad-main.s3.amazonaws.com/avatars/Swimmer04.svg',
+        'https://scribble-stadium.s3.amazonaws.com/avatars/Swimmer04.svg',
     },
     {
       AvatarURL:
-        'https://storysquad-main.s3.amazonaws.com/avatars/Swimmer05.svg',
+        'https://scribble-stadium.s3.amazonaws.com/avatars/Swimmer05.svg',
     },
     {
       AvatarURL:
-        'https://storysquad-main.s3.amazonaws.com/avatars/Swimmer06.svg',
+        'https://scribble-stadium.s3.amazonaws.com/avatars/Swimmer06.svg',
     },
     {
       AvatarURL:
-        'https://storysquad-main.s3.amazonaws.com/avatars/Swimmer07.svg',
+        'https://scribble-stadium.s3.amazonaws.com/avatars/Swimmer07.svg',
     },
     {
       AvatarURL:
-        'https://storysquad-main.s3.amazonaws.com/avatars/Swimmer08.svg',
+        'https://scribble-stadium.s3.amazonaws.com/avatars/Swimmer08.svg',
     },
     {
       AvatarURL:
-        'https://storysquad-main.s3.amazonaws.com/avatars/Swimmer09.svg',
+        'https://scribble-stadium.s3.amazonaws.com/avatars/Swimmer09.svg',
     },
     {
       AvatarURL:
-        'https://storysquad-main.s3.amazonaws.com/avatars/Swimmer10.svg',
+        'https://scribble-stadium.s3.amazonaws.com/avatars/Swimmer10.svg',
     },
     {
       AvatarURL:
-        'https://storysquad-main.s3.amazonaws.com/avatars/Swimmer11.svg',
+        'https://scribble-stadium.s3.amazonaws.com/avatars/Swimmer11.svg',
     },
     {
       AvatarURL:
-        'https://storysquad-main.s3.amazonaws.com/avatars/Swimmer12.svg',
+        'https://scribble-stadium.s3.amazonaws.com/avatars/Swimmer12.svg',
     },
     {
       AvatarURL:
-        'https://storysquad-main.s3.amazonaws.com/avatars/Swimmer13.svg',
+        'https://scribble-stadium.s3.amazonaws.com/avatars/Swimmer13.svg',
     },
     {
       AvatarURL:
-        'https://storysquad-main.s3.amazonaws.com/avatars/Swimmer14.svg',
+        'https://scribble-stadium.s3.amazonaws.com/avatars/Swimmer14.svg',
     },
     {
       AvatarURL:
-        'https://storysquad-main.s3.amazonaws.com/avatars/Swimmer15.svg',
+        'https://scribble-stadium.s3.amazonaws.com/avatars/Swimmer15.svg',
     },
     {
       AvatarURL:
-        'https://storysquad-main.s3.amazonaws.com/avatars/Swimmer16.svg',
+        'https://scribble-stadium.s3.amazonaws.com/avatars/Swimmer16.svg',
     },
   ]);
 };
