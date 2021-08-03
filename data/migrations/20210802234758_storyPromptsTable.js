5693563a7f933e73b504d269d787a5e4cc781ae0
ChadDiaz <chadcdiaz@gmail.com>
2021-08-02 23:50:09 -0400

    finished the export up function

--- data/migrations/20210802234758_storyPromptsTable.js
+++ data/migrations/20210802234758_storyPromptsTable.js
@@ -1,8 +1,13 @@
-
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.createTable('Story-Prompts', (sp) => {
+    sp.increments('ID');
+    sp.integer('EpisodeID')
+      .references('Episodes.ID')
+      .notNullable()
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+    sp.string('Type').notNullable();
+    sp.string('Prompt').notNullable();
+  });
 };
 
-exports.down = function(knex) {
-  
-};
