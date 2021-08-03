5f0f087f32187e1095b2148e113a7adb02f34cf9
Lukecp <lukecp@pm.me>
2021-08-03 00:04:57 -0400

    fixed linting errors

--- data/migrations/20210802234515_add-episodes-table.js
+++ data/migrations/20210802234515_add-episodes-table.js
@@ -1,19 +1,17 @@
 exports.up = function (knex) {
-  return knex.schema
-		.createTable('Episodes', (t) => {
-			t.increments('ID');
-			t.integer('StoryID')
-				.notNullable()
-				.unsigned()
-				.references('StoriesNew.ID')
-				.onDelete('CASCADE');
-			t.integer('EpisodeNumber');
-			t.string('TextURL');
-			t.string('AudioURL');
-	});
+  return knex.schema.createTable('Episodes', (t) => {
+    t.increments('ID');
+    t.integer('StoryID')
+      .notNullable()
+      .unsigned()
+      .references('StoriesNew.ID')
+      .onDelete('CASCADE');
+    t.integer('EpisodeNumber');
+    t.string('TextURL');
+    t.string('AudioURL');
+  });
 };
 
 exports.down = function (knex) {
-	return knex.schema
-		.dropTableIfExists('Episodes');
+  return knex.schema.dropTableIfExists('Episodes');
 };
