427685f9b9d93ac90f56766deb268c203c46ba2a
Chad Diaz <chad.diazmd@gmail.com>
2021-08-03 22:10:29 -0400

    Feature/StoryPromptsTable (#56)
    * created migration file
    
    * finished the export up function
    
    * finished the exports down function
    
    * changed file name to match convention

---
+++ data/migrations/20210802234758_story-prompts-table.js
@@ -0,0 +1,16 @@
+exports.up = function (knex) {
+  return knex.schema.createTable('Story-Prompts', (sp) => {
+    sp.increments('ID');
+    sp.integer('EpisodeID')
+      .references('Episodes.ID')
+      .notNullable()
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+    sp.string('Type').notNullable();
+    sp.string('Prompt').notNullable();
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('Story-Prompts');
+};
