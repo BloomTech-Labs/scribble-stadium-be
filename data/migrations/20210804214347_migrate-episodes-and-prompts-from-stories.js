cc7d147515953e92f7a92aebc96c83756db0b48c
Lukecp <lukecp@pm.me>
2021-08-05 00:16:34 -0400

    refactored for loop to a .forEach

--- data/migrations/20210804214347_migrate-episodes-and-prompts-from-stories.js
+++ data/migrations/20210804214347_migrate-episodes-and-prompts-from-stories.js
@@ -1,22 +1,17 @@
 exports.up = function (knex) {
   knex('Stories')
     .select('*')
-    .then(async (res) => {
-      //   console.log('this is stories', res);
-      for (let i = 0; i < res.length; i++) {
-        let EpisodeObj = {
-          StoryID: "1",
-          EpisodeNumber: res[i].ID,
-          TextURL: res[i].URL,
-          AudioURL: res[i].Audiofile,
-        };
-        await knex('Episodes').insert(EpisodeObj);
-      }
-    });
-  knex('Episodes')
-    .select('*')
     .then((res) => {
-      console.log('this is now episodes', res);
+      res.forEach(async (ep) => {
+        const EpisodeObj = {
+          StoryID: '1',
+          EpisodeNumber: ep.ID,
+          TextURL: ep.URL,
+          AudioURL: ep.Audiofile,
+        };
+        console.log(EpisodeObj);
+        await knex('Episodes').insert(EpisodeObj);
+      });
     });
 };
 
