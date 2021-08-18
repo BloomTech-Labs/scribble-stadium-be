61ac8d7e673db39004312d58ec96f878c2558ae7
Anil_Ramcharran <anil.gitme@gmail.com>
2021-08-18 00:44:08 -0400

    restoring app.js to fix lint errors

--- api/app.js
+++ api/app.js
@@ -12,12 +12,12 @@ const config_result = dotenv.config();
 // const scheduler = require('./cronTasks/scheduler');
 require('./cronTasks/notificationScheduler.js');
 if (process.env.NODE_ENV != 'production' && config_result.error) {
-    throw config_result.error;
+  throw config_result.error;
 }
 
 const swaggerSpec = swaggerJSDoc(jsdocConfig);
 const swaggerUIOptions = {
-    explorer: true,
+  explorer: true,
 };
 
 //###[  Routers ]###
@@ -37,19 +37,18 @@ const leadBoard = require('./leaderboard/leadboardRouter');
 const achievements = require('./Achievements/achieveRouter');
 const streaks = require('./Streaks/streaksRouter');
 const gallery = require('./Gallery/GalleryRouter');
-const singleplayerRouter = require('./singleplayer/singleplayerRouter');
 
 const app = express();
 
 process.on('unhandledRejection', (reason, p) => {
-    console.error('Unhandled Rejection at: Promise', p, 'reason:', reason);
-    // application specific logging, throwing an error, or other logic here
+  console.error('Unhandled Rejection at: Promise', p, 'reason:', reason);
+  // application specific logging, throwing an error, or other logic here
 });
 // docs would need to be built and committed
 app.use(
-    '/api-docs',
-    swaggerUi.serve,
-    swaggerUi.setup(swaggerSpec, swaggerUIOptions)
+  '/api-docs',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerSpec, swaggerUIOptions)
 );
 
 app.use(helmet());
@@ -76,33 +75,32 @@ app.use('/leaderboard', leadBoard);
 app.use('/achievements', achievements);
 app.use(['/streaks', 'streak'], streaks);
 app.use('/gallery', gallery);
-app.use('/singleplayer', singleplayerRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    next(createError(404));
+app.use(function (req, res, next) {
+  next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-    if (err instanceof createError.HttpError) {
-        res.locals.message = err.message;
-        res.locals.status = err.statusCode;
-        if (process.env.NODE_ENV === 'development') {
-            res.locals.error = err;
-        }
+app.use(function (err, req, res, next) {
+  if (err instanceof createError.HttpError) {
+    res.locals.message = err.message;
+    res.locals.status = err.statusCode;
+    if (process.env.NODE_ENV === 'development') {
+      res.locals.error = err;
     }
-    console.error(err);
-    if (process.env.NODE_ENV === 'production' && !res.locals.message) {
-        res.locals.message = 'ApplicationError';
-        res.locals.status = 500;
-    }
-    if (res.locals.status) {
-        res.status(res.locals.status || 500);
-        const errObject = { error: res.locals.error, message: res.locals.message };
-        return res.json(errObject);
-    }
-    next(err);
+  }
+  console.error(err);
+  if (process.env.NODE_ENV === 'production' && !res.locals.message) {
+    res.locals.message = 'ApplicationError';
+    res.locals.status = 500;
+  }
+  if (res.locals.status) {
+    res.status(res.locals.status || 500);
+    const errObject = { error: res.locals.error, message: res.locals.message };
+    return res.json(errObject);
+  }
+  next(err);
 });
 
 module.exports = app;
