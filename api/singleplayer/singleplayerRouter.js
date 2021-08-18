9ce565ad9c6b1c4641ee6bf033a2a3d8c0768374
Anil_Ramcharran <anil.gitme@gmail.com>
2021-08-18 02:01:43 -0400

    fixing lint error in single player router

--- api/singleplayer/singleplayerRouter.js
+++ api/singleplayer/singleplayerRouter.js
@@ -45,4 +45,5 @@ router.post('/savebot/:id', authRequired, fileUpload, async (req, res) => {
   crudOperationsManager.post(res, Singleplayers.add, 'Singleplayer', botdata);
 });
 
+
 module.exports = router;
