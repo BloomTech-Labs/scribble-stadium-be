640ac1c5efd513ba34b8e30bba717b976b0ec9a1
Jayaram Nair <jayaramsivaramannair@gmail.com>
2021-08-18 21:57:17 -0400

    Removed hashpin from index.js inside test folder and childRouter.js to fix errors while running tests

--- api/child/childRouter.js
+++ api/child/childRouter.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const { hashPin } = require('../../lib');
 
 const {
   authRequired,
@@ -254,11 +253,6 @@ router.put('/:id', authRequired, childUpdateValidation, (req, res) => {
   // Pull relevant data out of the request object
   const { id } = req.params;
   const changes = req.body;
-
-  // Check to see if PIN is provided. If it is, then let's hash the new pin before updating the profile.
-  if (changes.PIN) {
-    changes.PIN = hashPin(changes.PIN);
-  }
 
   crudOperationsManager.update(res, Children.update, 'Child', id, changes);
 });
