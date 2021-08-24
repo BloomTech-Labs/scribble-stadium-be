e27183296f0c7d265878821018f3cf95b42d71ec
Allison Castaneda <allraec.dev@gmail.com>
2021-08-24 02:50:43 -0500

    fixed lint errors

--- api/stories/newStoryRouter.js
+++ api/stories/newStoryRouter.js
@@ -3,9 +3,9 @@ const { crudOperationsManager } = require('../../lib');
 const Stories = require('./newStoryModel');
 
 router.get('/:id', (req, res) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    crudOperationsManager.getById(res, Stories.getStoryByID, 'Story', id);
+  crudOperationsManager.getById(res, Stories.getStoryByID, 'Story', id);
 });
 
 module.exports = router;
