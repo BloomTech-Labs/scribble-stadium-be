a045c3c38b2cdc8e17c88f77e68ea06257ea2adc
Kelsey Nielsen <kelseywnielsen@gmail.com>
2021-08-24 15:46:04 -0600

    replaced okta authorization with auth0 authorization

--- api/middleware/authRequired.js
+++ api/middleware/authRequired.js
@@ -1,46 +1,18 @@
-/* istanbul ignore file */
-const createError = require('http-errors');
-const OktaJwtVerifier = require('@okta/jwt-verifier');
-const oktaVerifierConfig = require('../../config/okta');
-const Parents = require('../parent/parentModel');
-const oktaJwtVerifier = new OktaJwtVerifier(oktaVerifierConfig.config);
+const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
 
-const makeParentObject = (claims) => {
-  return {
-    Email: claims.email,
-    Name: claims.name,
-  };
-};
-/**
- * A simple middleware that asserts valid Okta idToken and sends 401 responses
- * if the token is not present or fails validation. If the token is valid its
- * contents are attached to req.profile
- */
-const authRequired = async (req, res, next) => {
-  try {
-    const authHeader = req.headers.authorization || '';
-    const match = authHeader.match(/Bearer (.+)/);
+const authRequired = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
+  }),
 
-    if (!match) throw new Error('Missing idToken');
-
-    const idToken = match[1];
-
-    const data = await oktaJwtVerifier.verifyAccessToken(
-      idToken,
-      oktaVerifierConfig.expectedAudience
-    );
-
-    const jwtUserObj = makeParentObject(data.claims);
-    const parent = await Parents.findOrCreate(jwtUserObj);
-    if (parent) {
-      req.profile = parent;
-    } else {
-      throw new Error('Unable to process idToken');
-    }
-    next();
-  } catch (err) {
-    next(createError(401, err.message));
-  }
-};
+  // Validate the audience and the issuer.
+  audience: process.env.AUTH0_AUDIENCE,
+  issuer: `https://${process.env.AUTH0_DOMAIN}/`,
+  algorithms: ['RS256'],
+});
 
 module.exports = authRequired;
