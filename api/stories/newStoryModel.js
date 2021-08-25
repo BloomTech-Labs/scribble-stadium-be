16275052764805b3af5146e84534d43ded8ff2b1
Allison Castaneda <allraec.dev@gmail.com>
2021-08-24 20:30:10 -0500

    resolved to add comments for newStoryModel

--- api/stories/newStoryModel.js
+++ api/stories/newStoryModel.js
@@ -1,5 +1,10 @@
 const db = require('../../data/db-config');
 
+/**
+ * Queries the database for a specific story with given ID
+ * @param {number} ID the ID to search for in the database
+ * @returns {Promise} a promise that resolves to story object of the given story ID
+ */
 const getStoryByID = (ID) => {
   return db('Stories-New').where({ ID });
 };
