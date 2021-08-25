3eb529c5e6e72ece794b8214a0e6f6a10f6cc411
Kelsey Nielsen <kelseywnielsen@gmail.com>
2021-08-24 20:08:19 -0600

    added custom next function to access and add parent email and name to database

--- api/middleware/authRequired.js
+++ api/middleware/authRequired.js
@@ -1,7 +1,53 @@
 const jwt = require('express-jwt');
 const jwksRsa = require('jwks-rsa');
+const createError = require('http-errors');
+const Parents = require('../parent/parentModel');
 
-const authRequired = jwt({
+const makeParentObject = (claims) => {
+  return {
+    Email: claims.email,
+    Name: claims.name,
+  };
+};
+
+const authRequired = async (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization || '';
+    const match = authHeader.match(/Bearer (.+)/);
+
+    if (!match) throw new Error('Missing idToken');
+
+    const idToken = match[1];
+    console.log("idToken", idToken)
+    jwt({
+      secret: jwksRsa.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
+      }),
+    
+      // Validate the audience and the issuer.
+      audience: process.env.AUTH0_AUDIENCE,
+      issuer: `https://${process.env.AUTH0_DOMAIN}/`,
+      algorithms: ['RS256'],
+    })(req, res, async ()=> {
+      console.log("jwt res.user", req.user);
+      const jwtUserObj = makeParentObject(data.claims);
+      const parent = await Parents.findOrCreate(jwtUserObj);
+      if (parent) {
+        req.profile = parent;
+      } else {
+        throw new Error('Unable to process idToken');
+      }
+      next.apply(this, arguments);
+    });
+  } catch (err) {
+    next(createError(401, err.message));
+  }
+};
+
+const authRequired1 = jwt({
   secret: jwksRsa.expressJwtSecret({
     cache: true,
     rateLimit: true,
