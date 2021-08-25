b915a44273515d5ba4c7577ef1dc2e914adf5aff
Allison Castaneda <38802056+allraec@users.noreply.github.com>
2021-08-25 16:38:33 -0500

    Added GET endpoint for newStoryModel and newStoryRouter for new stories schema (#68)
    * added getStory model and router
    
    * fixed npm run lint
    
    * removed unused swagger comment
    
    * updated router to use crud operation manager
    
    * fixed lint errors
    
    * removed verbose code
    
    * minor lint error fix
    
    * resolved to add comments for newStoryModel
    
    Co-authored-by: Allison Castaneda <allraec.dev@gmail.com>

---
+++ api/stories/newStoryRouter.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const { crudOperationsManager } = require('../../lib');
+const Stories = require('./newStoryModel');
+
+router.get('/:id', (req, res) => {
+  // Pull story ID out of the URL params
+  const { id } = req.params;
+
+  crudOperationsManager.getById(res, Stories.getStoryByID, 'Story', id);
+});
+
+module.exports = router;
