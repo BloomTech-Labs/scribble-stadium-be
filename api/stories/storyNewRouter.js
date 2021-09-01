ae501adb1197e217843f94a43949c3323e2891e7
Allison Castaneda <allraec.dev@gmail.com>
2021-08-31 20:22:23 -0500

    Added endpoints to GET, POST & DELETE story object

--- api/stories/storyNewRouter.js
+++ api/stories/storyNewRouter.js
@@ -2,11 +2,33 @@ const router = require('express').Router();
 const { crudOperationsManager } = require('../../lib');
 const Stories = require('./storyNewModel');
 
+router.post('/', (req, res) => {
+  // Pull relevant data out of the request object
+  const newStory = req.body;
+
+  crudOperationsManager.post(res, Stories.add, 'Story', newStory);
+});
+
 router.get('/:id', (req, res) => {
   // Pull story ID out of the URL params
   const { id } = req.params;
 
   crudOperationsManager.getById(res, Stories.getById, 'Story', id);
+});
+
+router.put('/:id', (req, res) => {
+  // Pull relevant data out of the request object
+  const { id } = req.params;
+  const changes = req.body;
+
+  crudOperationsManager.update(res, Stories.update, 'Story', id, changes);
+});
+
+router.delete('/:id', (req, res) => {
+  // Pull story ID out of the URL params
+  const { id } = req.params;
+
+  crudOperationsManager.update(res, Stories.remove, 'Story', id);
 });
 
 module.exports = router;
