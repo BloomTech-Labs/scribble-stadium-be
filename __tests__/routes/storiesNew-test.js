069dbba855f29ecbe1b757acad4523ab951a7387
Allison Castaneda <allraec.dev@gmail.com>
2021-08-31 20:23:43 -0500

    Added GET endpoint test for new story schema

---
+++ __tests__/routes/storiesNew-test.js
@@ -0,0 +1,18 @@
+const request = require('supertest');
+
+const {
+  storiesNew,
+} = require('../../data/testdata');
+
+module.exports = () => {
+  describe('story router endpoints', () => {
+    describe('GET /storyNew/:id', () => {
+      test('should return the newly created story', async () => {
+        const res = await request(server).get('/storyNew/1');
+
+        expect(res.status).toBe(200);
+        expect(res.body).toEqual({ ID: 1, ...storiesNew[0] });
+      });
+    });
+  });
+};
