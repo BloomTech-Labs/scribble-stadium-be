3c190cdf4388fac5af8264b11b095d817f2e5a05
Kelsey Nielsen <kelseywnielsen@gmail.com>
2021-09-03 21:04:04 -0600

    commented out code that applies to Parents database until we figure out what data we are working with

--- api/middleware/authRequired.js
+++ api/middleware/authRequired.js
@@ -1,25 +1,22 @@
 const jwt = require('express-jwt');
 const jwksRsa = require('jwks-rsa');
 const createError = require('http-errors');
-const Parents = require('../parent/parentModel');
+// const Parents = require('../parent/parentModel');
 
-const makeParentObject = (claims) => {
-  return {
-    Email: claims.email,
-    Name: claims.name,
-  };
-};
+// const makeParentObject = (claims) => {
+//   return {
+//     Email: claims.email,
+//     Name: claims.name,
+//   };
+// };
 
 const authRequired = async (req, res, next) => {
+  //write anonymous func for next
+  //get jwt to work
+  //maybe fix next function
   try {
-    const authHeader = req.headers.authorization || '';
-    const match = authHeader.match(/Bearer (.+)/);
-
-    if (!match) throw new Error('Missing idToken');
-
-    const idToken = match[1];
-    console.log("idToken", idToken)
-    jwt({
+    console.log("jwt", jwt)
+    const response = jwt(req, res, next, {
       secret: jwksRsa.expressJwtSecret({
         cache: true,
         rateLimit: true,
@@ -31,34 +28,26 @@ const authRequired = async (req, res, next) => {
       audience: process.env.AUTH0_AUDIENCE,
       issuer: `https://${process.env.AUTH0_DOMAIN}/`,
       algorithms: ['RS256'],
-    })(req, res, async ()=> {
-      console.log("jwt res.user", req.user);
-      const jwtUserObj = makeParentObject(data.claims);
-      const parent = await Parents.findOrCreate(jwtUserObj);
-      if (parent) {
-        req.profile = parent;
-      } else {
-        throw new Error('Unable to process idToken');
-      }
-      next.apply(this, arguments);
+    })
+    console.log(response)
+
+    const output = response(req, res, async ()=> {
+      console.log("jwt req.user", req.user);
+      // const jwtUserObj = makeParentObject(data.claims);
+      // const parent = await Parents.findOrCreate(jwtUserObj);
+      // if (parent) {
+      //   req.profile = parent;
+      // } else {
+      //   throw new Error('Unable to process idToken');
+      // }
+      // next.apply(this, arguments);
     });
+    console.log("output", output)
+    next.apply(req, res)
+
   } catch (err) {
     next(createError(401, err.message));
   }
 };
-
-const authRequired1 = jwt({
-  secret: jwksRsa.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
-  }),
-
-  // Validate the audience and the issuer.
-  audience: process.env.AUTH0_AUDIENCE,
-  issuer: `https://${process.env.AUTH0_DOMAIN}/`,
-  algorithms: ['RS256'],
-});
 
 module.exports = authRequired;
