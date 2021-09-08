2fbf0df70400fcf46c4be169c07a5be4d6e8d29a
Kelsey Nielsen <kelseywnielsen@gmail.com>
2021-09-07 20:02:30 -0600

    implemented a new express hook. Changed the audience to client id.

--- api/middleware/authRequired.js
+++ api/middleware/authRequired.js
@@ -1,6 +1,9 @@
 const jwt = require('express-jwt');
 const jwksRsa = require('jwks-rsa');
 const createError = require('http-errors');
+const Express = require('express');
+const app = new Express();
+
 // const Parents = require('../parent/parentModel');
 
 // const makeParentObject = (claims) => {
@@ -11,43 +14,40 @@ const createError = require('http-errors');
 // };
 
 const authRequired = async (req, res, next) => {
-  //write anonymous func for next
-  //get jwt to work
-  //maybe fix next function
   try {
-    console.log("jwt", jwt)
-    const response = jwt(req, res, next, {
+    app.use(jwt({
+      // Dynamically provide a signing key based on the kid in the header and the signing keys provided by the JWKS endpoint.
       secret: jwksRsa.expressJwtSecret({
         cache: true,
         rateLimit: true,
         jwksRequestsPerMinute: 5,
-        jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
+        jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
       }),
-    
+
       // Validate the audience and the issuer.
-      audience: process.env.AUTH0_AUDIENCE,
+      audience: process.env.AUTH0_CLIENT_ID,
       issuer: `https://${process.env.AUTH0_DOMAIN}/`,
-      algorithms: ['RS256'],
-    })
-    console.log(response)
-
-    const output = response(req, res, async ()=> {
-      console.log("jwt req.user", req.user);
-      // const jwtUserObj = makeParentObject(data.claims);
-      // const parent = await Parents.findOrCreate(jwtUserObj);
-      // if (parent) {
-      //   req.profile = parent;
-      // } else {
-      //   throw new Error('Unable to process idToken');
-      // }
-      // next.apply(this, arguments);
+      algorithms: [ 'RS256' ]
+    }))
+    (req, res, async () => {
+            console.log("jwt req.user", await req.user);
+            // const jwtUserObj = makeParentObject(data.claims);
+            // const parent = await Parents.findOrCreate(jwtUserObj);
+            // if (parent) {
+            //   req.profile = parent;
+            // } else {
+            //   throw new Error('Unable to process idToken');
+            // }
+            // next.apply(this, arguments);
     });
-    console.log("output", output)
+          
     next.apply(req, res)
-
+    
   } catch (err) {
     next(createError(401, err.message));
   }
 };
+
+
 
 module.exports = authRequired;
