c724c36bacb55dd07f73e9c9582c1648e7304742
Rezaur Rahman <rezaur.rahman_nyc@icloud.com>
2021-09-09 10:27:42 -0400

    Full CRUD operation added for Episode

--- api/stories/storyNewRouter.js
+++ api/stories/storyNewRouter.js
@@ -35,6 +35,36 @@ router.get('/:id/episodes', async (req, res) => {
   // Pull story ID out of the URL params
   const { id } = req.params;
   crudOperationsManager.getAll(res, Stories.getEpisodesByStoryID, 'Story', id);
+})
+
+router.get('/:id/episode/:eid', (req, res) => {
+  const { id ,eid } = req.params
+
+  crudOperationsManager.getById(res, Stories.getEpisodeByID, 'Episode', id, eid);
 });
+
+router.post('/:id/episodes', (req, res) => {
+  // Pull relevant data out of the request object
+  const { id } = req.params
+  const newEpisode = req.body
+  crudOperationsManager.post(res, Stories.addEpisode, 'Story', id, newEpisode);
+})
+
+router.put('/:id/episode/:eid', (req, res) => {
+  // Pull relevant data out of the request object
+  const { id, eid } = req.params
+  const changes = req.body
+
+  crudOperationsManager.update(res, Stories.updateEpisode, 'Episode', id, eid, changes)
+})
+
+router.delete('/:id/episode/:eid', (req, res) => {
+  // Pull story ID out of the URL params
+  const { id, eid } = req.params;
+
+  crudOperationsManager.update(res, Stories.removeEpisode, 'Episode', id, eid)
+});
+
+
 
 module.exports = router;
