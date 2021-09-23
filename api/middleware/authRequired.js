5dda39e558c2a27d04c5c00419701bda47e7c2dd
Kelsey Nielsen <kelseywnielsen@gmail.com>
2021-09-22 19:01:45 -0600

    moved auth0 verification function to app.js in order to catch errors correctly

--- api/middleware/authRequired.js
+++ api/middleware/authRequired.js
@@ -1,9 +1,4 @@
-const jwt = require('express-jwt');
-const jwksRsa = require('jwks-rsa');
 const createError = require('http-errors');
-const Express = require('express');
-const app = new Express();
-
 const Parents = require('../parent/parentModel');
 
 const makeParentObject = (user) => {
@@ -15,31 +10,14 @@ const makeParentObject = (user) => {
 
 const authRequired = async (req, res, next) => {
   try {
-    app.use(
-      jwt({
-        // Dynamically provide a signing key based on the kid in the header and the signing keys provided by the JWKS endpoint.
-        secret: jwksRsa.expressJwtSecret({
-          cache: true,
-          rateLimit: true,
-          jwksRequestsPerMinute: 5,
-          jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
-        }),
-
-        // Validate the audience and the issuer.
-        audience: process.env.AUTH0_CLIENT_ID,
-        issuer: `https://${process.env.AUTH0_DOMAIN}/`,
-        algorithms: ['RS256'],
-      })
-    )(req, res, async () => {
-      const jwtUserObj = makeParentObject(req.user);
-      const parent = await Parents.findOrCreate(jwtUserObj);
-      if (parent) {
-        req.profile = parent;
-        next.apply(req, res);
-      } else {
-        throw new Error('Unable to process idToken');
-      }
-    });
+    const jwtUserObj = makeParentObject(req.auth0User);
+    const parent = await Parents.findOrCreate(jwtUserObj);
+    if (parent) {
+      req.profile = parent;
+      next();
+    } else {
+      throw new Error('Unable to process idToken');
+    }
   } catch (err) {
     next(createError(401, err.message));
   }
