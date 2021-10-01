cef54e77f3631f6bbfeb7d6dbe3a5d411e5dbfaa
Jake Mallory <4095+tinomen@users.noreply.github.com>
2021-09-30 18:10:18 -0600

    add create_test_db script

---
+++ data/create_test_db.js
@@ -0,0 +1,9 @@
+const db = require('./db-config');
+
+db.raw('CREATE DATABASE api-test;')
+  .then(function(result) {
+    console.log('Database `api-test`created\n', result);
+  })
+  .finally(function () {
+    console.log("Done");
+  });
