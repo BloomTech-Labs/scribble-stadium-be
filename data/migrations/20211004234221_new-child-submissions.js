97b94e3f2e3db60ae14a7a1e59d985b69a511fba
Alan Mir <alanpaulphotos@gmail.com>
2021-10-05 00:15:36 -0400

    Created new migrations table for submissions

---
+++ data/migrations/20211004234221_new-child-submissions.js
@@ -0,0 +1,77 @@
+exports.up = function (knex) {
+    return knex.schema
+      .createTable('NewSubmissions', (t) => {
+        t.increments('ID');
+        t.integer('ChildID')
+            .notNullable()
+            .unsigned()
+            .references('Children.ID')
+            .onUpdate('CASCADE')
+            .onDelete('RESTRICT');
+        t.integer('StoryID')
+            .notNullable()
+            .unsigned()
+            .references('Stories.ID')
+            .onUpdate('CASCADE')
+            .onDelete('RESTRICT');
+        t.integer('CohortId')
+            .references('Cohorts.ID')
+            .notNullable()
+            .unsigned()
+            .onUpdate('CASCADE')
+            .onDelete('RESTRICT');
+        t.date('EpisodeEndDate')
+            .notNullable()
+            .unsigned();
+        t.string('ModerationStatus');
+
+        t.boolean('HasRead').defaultTo(false);
+        t.boolean('HasWritten').defaultTo(false);
+        t.boolean('HasDrawn').defaultTo(false);
+        
+        t.integer('Complexity');
+        t.boolean('LowConfidence').defaultTo(false);
+        t.timestamp('CreatedAt');
+        t.timestamp('UpdatedAt');
+
+        t.timestamp("StoryRead");
+        t.timestamp('WritingPrompt');
+        t.timestamp('DrawingPrompt');
+
+        t.boolean('MeetsDeadline').defaultTo(false);
+        t.boolean('PublicDisplayed').defaultTo(true);
+
+        t.unique(['ChildID', 'StoryID']);
+      })
+    //   .createTable('Writing', (t) => {
+    //     t.increments('ID');
+    //     t.string('URL').notNullable().unique();
+    //     t.integer('PageNum').notNullable();
+    //     t.integer('SubmissionID')
+    //       .notNullable()
+    //       .unsigned()
+    //       .references('Submissions.ID')
+    //       .onUpdate('CASCADE')
+    //       .onDelete('RESTRICT');
+    //     t.unique(['PageNum', 'SubmissionID']);
+    //   })
+    //   .createTable('Drawing', (t) => {
+    //     t.increments('ID');
+    //     t.string('URL').notNullable().unique();
+    //     t.integer('SubmissionID')
+    //       .notNullable()
+    //       .unsigned()
+    //       .unique()
+    //       .references('Submissions.ID')
+    //       .onUpdate('CASCADE')
+    //       .onDelete('RESTRICT');
+    //   });
+  };
+  
+  exports.down = function (knex) {
+    return knex.schema
+    //   .dropTableIfExists('Drawing')
+    //   .dropTableIfExists('Writing')
+      .dropTableIfExists('Submissions');
+  };
+  
