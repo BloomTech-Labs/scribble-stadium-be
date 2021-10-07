3ed138fcfb9eed42d26c162b5d87fbb46cd83873
Avery Burns <a.aburns95@gmail.com>
2021-10-07 00:48:12 -0500

    changed table name to submissionsNew, removed boolean columns from migrtns & seed data, added started&finishedReadingAt columns, edited all columns to use camel case

--- data/migrations/20211004234221_new-child-submissions.js
+++ data/migrations/20211004234221_new-child-submissions.js
@@ -1,62 +1,31 @@
 exports.up = function (knex) {
-    return knex.schema
-      .createTable('NewSubmissions', (t) => {
-        t.increments('ID');
-        t.integer('ChildID')
-            .notNullable()
-            .unsigned()
-            .references('Children.ID')
-            .onUpdate('CASCADE')
-            .onDelete('RESTRICT');
-        t.integer('StoryID')
-            .notNullable()
-            .unsigned()
-            .references('Stories.ID')
-            .onUpdate('CASCADE')
-            .onDelete('RESTRICT');
-        t.date('EpisodeStartDate')
-            .notNullable()
-            .unsigned();
-        t.string('ModerationStatus');
+  return knex.schema.createTable('submissionsNew', (t) => {
+    t.increments('id');
+    t.integer('childId')
+      .notNullable()
+      .unsigned()
+      .references('Children.ID')
+      .onUpdate('CASCADE')
+      .onDelete('RESTRICT');
+    t.integer('storyId')
+      .notNullable()
+      .unsigned()
+      .references('Stories.ID')
+      .onUpdate('CASCADE')
+      .onDelete('RESTRICT');
+    t.date('episodeStartDate').notNullable().unsigned();
+    t.string('moderationStatus');
 
-        t.boolean('HasRead').defaultTo(false);
-        t.boolean('HasWritten').defaultTo(false);
-        t.boolean('HasDrawn').defaultTo(false);
-        
-        t.integer('Complexity');
-        t.boolean('LowConfidence').defaultTo(false);
-        t.timestamp('CreatedAt').defaultTo(knex.fn.now());
-        t.timestamp('UpdatedAt').defaultTo(knex.fn.now());
-      })
-    //   .createTable('Writing', (t) => {
-    //     t.increments('ID');
-    //     t.string('URL').notNullable().unique();
-    //     t.integer('PageNum').notNullable();
-    //     t.integer('SubmissionID')
-    //       .notNullable()
-    //       .unsigned()
-    //       .references('Submissions.ID')
-    //       .onUpdate('CASCADE')
-    //       .onDelete('RESTRICT');
-    //     t.unique(['PageNum', 'SubmissionID']);
-    //   })
-    //   .createTable('Drawing', (t) => {
-    //     t.increments('ID');
-    //     t.string('URL').notNullable().unique();
-    //     t.integer('SubmissionID')
-    //       .notNullable()
-    //       .unsigned()
-    //       .unique()
-    //       .references('Submissions.ID')
-    //       .onUpdate('CASCADE')
-    //       .onDelete('RESTRICT');
-    //   });
-  };
-  
-  exports.down = function (knex) {
-    return knex.schema
-    //   .dropTableIfExists('Drawing')
-    //   .dropTableIfExists('Writing')
-      .dropTableIfExists('NewSubmissions');
-  };
-  
+    t.timestamp('startedReadingAt');
+    t.timestamp('finishedReadingAt');
+
+    t.integer('complexity');
+    t.boolean('lowConfidence').defaultTo(false);
+    t.timestamp('createdAt').defaultTo(knex.fn.now());
+    t.timestamp('updatedAt').defaultTo(knex.fn.now());
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('submissionsNew');
+};
--- data/seeds/019_Submissions_New.js
+++ data/seeds/019_Submissions_New.js
@@ -1,72 +1,67 @@
 // Todo: need to have a range of complexity. fine for now though.
 
-// Creates a mock timestamp for CreatedAt and UpdatedAt
+// Creates a mock timestamp for createdAt and updatedAt
 const getTimeStamp = () => {
-  return new Date().toISOString().replace('Z','').replace('T', ' ');
-}
+  return new Date().toISOString().replace('Z', '').replace('T', ' ');
+};
 
 //FYI, each submission number here represents a week that was completed by a number of students.
-//So, cohorts 1,2&3 finished week1. 3 cohorts of 8 students = 24 items in this batch
+//So, *groups 1,2&3 finished week1. 3 groups of 8 students = 24 items in this batch
 const submissions1 = [...new Array(24)].map((i, idx) => ({
-  ChildID: `${idx + 1}`,
-  StoryID: 1,
-  EpisodeStartDate: '2021-01-03',
-  // ModerationStatus replaced Status in previous seed 
-  ModerationStatus: "PENDING",
-  // HasRead, HasWritten, and HasDrawn are all booleans to keep as check marks on what the child has done.
-  HasRead: true,
-  HasWritten: true,
-  HasDrawn: true,
-  Complexity: 30,
-  LowConfidence: false,
-  // Timestamps for CreatedAt, and UpdatedAt to capture data of when activity was last seen
-  CreatedAt: `${getTimeStamp()}`,
-  UpdatedAt: `${getTimeStamp()}`,
+  childId: `${idx + 1}`,
+  storyId: 1,
+  episodeStartDate: '2021-01-03',
+  // moderationStatus replaced Status in previous seed
+  moderationStatus: 'PENDING',
+  startedReadingAt: `${getTimeStamp()}` + 60000,
+  finishedReadingAt: `${getTimeStamp()}` + 15 * 60000,
+  complexity: 30,
+  lowConfidence: false,
+  // Timestamps for createdAt, and updatedAt to capture data of when activity was last seen
+  createdAt: `${getTimeStamp()}`,
+  updatedAt: `${getTimeStamp()}`,
 }));
 
-//So, cohorts 1&2 finished week2. 2 cohorts of 8 students = 16 items in this batch
+//So, groups 1&2 finished week2. 2 groups of 8 students = 16 items in this batch
 const submissions2 = [...new Array(16)].map((i, idx) => ({
-  ChildID: `${idx + 1}`,
-  StoryID: 2,
-  EpisodeStartDate: '2021-01-10',
-  ModerationStatus: "PENDING",
-  HasRead: true,
-  HasWritten: true,
-  HasDrawn: true,
-  Complexity: 30,
-  LowConfidence: false,
-  CreatedAt: `${getTimeStamp()}`,
-  UpdatedAt: `${getTimeStamp()}`
+  childId: `${idx + 1}`,
+  storyId: 2,
+  episodeStartDate: '2021-01-10',
+  moderationStatus: 'PENDING',
+  startedReadingAt: `${getTimeStamp()}` + 60000,
+  finishedReadingAt: `${getTimeStamp()}` + 15 * 60000,
+  complexity: 30,
+  lowConfidence: false,
+  createdAt: `${getTimeStamp()}`,
+  updatedAt: `${getTimeStamp()}`,
 }));
 
-//So, cohorts 1&2 finished week3. 2 cohorts of 8 students = 16 items in this batch
+//So, groups 1&2 finished week3. 2 groups of 8 students = 16 items in this batch
 const submissions3 = [...new Array(16)].map((i, idx) => ({
-  ChildID: `${idx + 1}`,
-  StoryID: 3,
-  EpisodeStartDate: '2021-01-17',
-  ModerationStatus: "PENDING",
-  HasRead: true,
-  HasWritten: true,
-  HasDrawn: true,
-  Complexity: 30,
-  LowConfidence: false,
-  CreatedAt: `${getTimeStamp()}`,
-  UpdatedAt: `${getTimeStamp()}`
+  childId: `${idx + 1}`,
+  storyId: 3,
+  episodeStartDate: '2021-01-17',
+  moderationStatus: 'PENDING',
+  startedReadingAt: `${getTimeStamp()}` + 60000,
+  finishedReadingAt: `${getTimeStamp()}` + 15 * 60000,
+  complexity: 30,
+  lowConfidence: false,
+  createdAt: `${getTimeStamp()}`,
+  updatedAt: `${getTimeStamp()}`,
 }));
 
-//Only cohorts 1 finished week4. 1 cohorts of 8 students = 8 items in this batch
+//Only groups 1 finished week4. 1 groups of 8 students = 8 items in this batch
 const submissions4 = [...new Array(8)].map((i, idx) => ({
-  ChildID: `${idx + 1}`,
-  StoryID: 4,
-  EpisodeStartDate: '2021-01-24',
-  ModerationStatus: "PENDING",
-  HasRead: true,
-  HasWritten: true,
-  HasDrawn: true,
-  Complexity: 30,
-  LowConfidence: false,
-  CreatedAt: `${getTimeStamp()}`,
-  UpdatedAt: `${getTimeStamp()}`
+  childId: `${idx + 1}`,
+  storyId: 4,
+  episodeStartDate: '2021-01-24',
+  moderationStatus: 'PENDING',
+  startedReadingAt: `${getTimeStamp()}` + 60000,
+  finishedReadingAt: `${getTimeStamp()}` + 15 * 60000,
+  complexity: 30,
+  lowConfidence: false,
+  createdAt: `${getTimeStamp()}`,
+  updatedAt: `${getTimeStamp()}`,
 }));
 
 const submissions = submissions1.concat(
@@ -77,5 +72,5 @@ const submissions = submissions1.concat(
 
 exports.seed = function (knex) {
   // Inserts seed entries
-  return knex('NewSubmissions').insert(submissions);
+  return knex('submissionsNew').insert(submissions);
 };
