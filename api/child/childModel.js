cd53cdf8602306f178882e44b3fbf7c6b444e353
Tony Sorensen <tony48853@gmail.com>
2021-10-13 14:19:33 -0700

    added CRUD operations and routes for submissions, updated swagger documentation in childRouter. tested in Insomnia and working. Co-authored-by: Aaron Burns a.aburns95@gmail.com

--- api/child/childModel.js
+++ api/child/childModel.js
@@ -115,6 +115,48 @@ const getComplexityList = (ChildID) => {
     .select(['ID', 'Complexity']);
 };
 
+/**
+ * This function checks to see if a submission exists at the given child/story intersection,
+ * and will create one if it does not.
+ * @param {number} childId the ID of the child who is submitting
+ * @param {number} storyId the ID of the story the submission is for
+ * @returns {Promise} returns a promise that resolves to a submission object
+ */
+const getSubmissionBySubId = async (id) => {
+  const foundSubmission = await db('Submissions-New').where({
+    id,
+  });
+  return foundSubmission;
+};
+
+/**
+ * This will query the database for a list of all of a child's submissions.
+ * @param {number} childId integer key of the requested child
+ * @returns {Array} a list of submission objects
+ */
+const getAllSubmissions = (childId) => {
+  return db.transaction(async (trx) => {
+    const subs = await trx('Submissions-New').where({ childId });
+    const res = subs.map((sub) => ({
+      ...sub,
+    }));
+    return res;
+  });
+};
+
+const addSubmission = (submission) => {
+  return db('Submissions-New').insert(submission);
+};
+
+const updateSubmissionBySubId = async (id, changes) => {
+  const updatedSub = await db('Submissions-New').where({ id }).update(changes);
+  return updatedSub;
+};
+
+const removeSubmission = (id) => {
+  return db('Submissions-New').where({ id }).del();
+};
+
 module.exports = {
   getAll,
   getById,
@@ -122,4 +164,9 @@ module.exports = {
   update,
   remove,
   getComplexityList,
+  getSubmissionBySubId,
+  getAllSubmissions,
+  addSubmission,
+  updateSubmissionBySubId,
+  removeSubmission,
 };
--- api/child/childRouter.js
+++ api/child/childRouter.js
@@ -140,9 +140,7 @@ router.get('/', authRequired, (req, res) => {
  *        $ref: '#/components/responses/DatabaseError'
  */
 
-//!!put authRequired back before pull request!!
-// router.get('/:id', authRequired, (req, res) => {
-router.get('/:id', (req, res) => {
+router.get('/:id', authRequired, (req, res) => {
   // Pull child ID out of the URL params
   const { id } = req.params;
 
@@ -283,6 +281,209 @@ router.delete('/:id', authRequired, (req, res) => {
   const { id } = req.params;
 
   crudOperationsManager.update(res, Children.remove, 'Child', id);
+});
+
+/**
+ * Schemas for submission data types.
+ * @swagger
+ * components:
+ *  schemas:
+ *    SubmissionStatus:
+ *      type: object
+ *      properties:
+ *        id:
+ *          type: integer
+ *        childId:
+ *          type: integer
+ *        storyId:
+ *          type: integer
+ *        episodeId:
+ *          type: integer
+ *        episodeStartDate:
+ *          type: date
+ *        moderationStatus:
+ *          type: string
+ *        startedReadingAt:
+ *          type: timestamp
+ *        finishedReadingAt:
+ *          type: timestamp
+ *        complexity:
+ *          type: integer
+ *        lowConfidence:
+ *          type: boolean
+ *        createdAt:
+ *          type: timestamp
+ *        updatedAt:
+ *          type: timestamp
+ *      example:
+ *        ID: 1
+ *        childId: 1
+ *        storyId: 1
+ *        episodeId: 1
+ *        episodeStartDate: '2021-01-03'
+ *        moderationStatus: not received
+ *        startedReadingAt: null
+ *        finishedReadingAt: null
+ *        complexity: 1
+ *        lowConfidence: false
+ *        createdAt: 2021-10-08 19:13:54.822+00
+ *        updatedAt: 2021-10-08 19:13:54.822+00
+ *
+ *  parameters:
+ *    submissionId:
+ *      name: Submission ID
+ *      in: path
+ *      description: ID of the desired submission
+ *      required: true
+ *      example: 1
+ *      schema:
+ *        type: integer
+ */
+
+/**
+ * @swagger
+ * /child/{id}/submissions/{id}:
+ *  get:
+ *    summary: Queries the database for information for the given submission.
+ *    description: Attempts to query the database to find a submission entry at the intersection
+ *      of the given Child ID and Story ID. If none is found, one will be created and initialized
+ *      with a childId, storyId, episodeId, episodeStartDate, and createdAt timestamp.
+ *    security:
+ *      - auth0: []
+ *    tags:
+ *      - Submissions
+ *    parameters:
+ *      - in: query
+ *        name: childId
+ *        schema:
+ *          type: integer
+ *        required: true
+ *        description: numeric ID of child
+ *      - in: query
+ *        name: storyId
+ *        schema:
+ *          type: integer
+ *        required: true
+ *        description: numeric ID of story
+ *      - in: query
+ *        name: episodeId
+ *        schema:
+ *          type: integer
+ *        required: true
+ *        description: numeric ID of episode
+ *      - in: query
+ *        name: episodeStartDate
+ *        schema:
+ *          type: date
+ *        required: true
+ *        description: start date of current episode
+ *      - in: query
+ *        name: createdAt
+ *        schema:
+ *          type: timestamp
+ *        required: true
+ *        description: when the submission record was created
+ *    responses:
+ *      200:
+ *        description: Returns an object containing the data for the requested submission
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/SubmissionStatus'
+ *      400:
+ *        $ref: '#/components/responses/MissingParameters'
+ *      401:
+ *        $ref: '#/components/responses/UnauthorizedError'
+ *      404:
+ *        $ref: '#/components/responses/InvalidID'
+ *      500:
+ *        $ref: '#/components/responses/DatabaseError'
+ */
+
+/**
+ * @swagger
+ * /child/{id}/submissions:
+ *  get:
+ *    summary: Attempts to get all data for every submission by a given child
+ *    security:
+ *      - auth0: []
+ *    tags:
+ *      - Submissions
+ *    parameters:
+ *      - $ref: '#/components/parameters/childId'
+ *    responses:
+ *      200:
+ *        description: Returns an array of all submissions by a child
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                $ref: '#/components/schemas/FullSubmission'
+ *      401:
+ *        $ref: '#/components/responses/UnauthorizedError'
+ *      500:
+ *        $ref: '#/components/responses/DatabaseError'
+ */
+router.get('/:id/submissions', authRequired, async (req, res) => {
+  // Pull child ID out of URL parameter
+  const { id } = req.params;
+
+  crudOperationsManager.getAll(
+    res,
+    Children.getAllSubmissions,
+    'Submission',
+    id
+  );
+});
+
+router.get('/:id/submissions/:id', authRequired, async (req, res) => {
+  //Pull submission ID out of URL parameter
+  const { id } = req.params;
+
+  crudOperationsManager.getAll(
+    res,
+    Children.getSubmissionBySubId,
+    'Submissions',
+    id
+  );
+});
+
+router.post('/:id/submissions', authRequired, (req, res) => {
+  //childId, storyId, episodeId, episodeStartDate required in body from front-end
+  const newSubmission = req.body;
+
+  crudOperationsManager.post(
+    res,
+    Children.addSubmission,
+    'Submission',
+    newSubmission
+  );
+});
+router.put('/:id/submissions/:id', authRequired, async (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+
+  crudOperationsManager.update(
+    res,
+    Children.updateSubmissionBySubId,
+    'Submission',
+    id,
+    changes
+  );
+});
+
+//Delete route for dev/admin purposes
+router.delete('/:id/submissions/:id', authRequired, async (req, res) => {
+  //Pull submission ID out of URL parameter
+  const { id } = req.params;
+
+  crudOperationsManager.update(
+    res,
+    Children.removeSubmission,
+    'Submission',
+    id
+  );
 });
 
 module.exports = router;
