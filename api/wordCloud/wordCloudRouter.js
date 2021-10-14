dc6ca8f9057b18faa0e516e97f5ba7487512b055
INFOCORP\Casey.Dietz <caseydietz20@gmail.com>
2021-10-13 21:15:23 -0600

    Created word cloud router

--- api/wordCloud/wordCloudRouter.js
+++ api/wordCloud/wordCloudRouter.js
@@ -1,50 +1,25 @@
 const { default: axios } = require('axios');
-
 const wordCloudRouter = require('express').Router();
 
-wordCloudRouter.get('/api/wordcloud', async (req, res) => {
+wordCloudRouter.get('/', async (req, res, next) => {
   try {
-    const res = await axios.post(process.env.WORDCLOUD_URL, {
-      headers: {
-        Authorization: `${process.env.DS_SECRET_TOKEN}`,
-      },
-      body: {
+    const result = await axios.post(
+      `${process.env.WORDCLOUD_URL}`,
+      {
         user_id: 'XiChi',
-        data_range: ['2019-06-16', '2020-10-23'],
-        complexity_metric: 'syl',
-        image_format: '.webp',
-        canvas_width: 960,
-        density: 0.4,
-        max_words: 200,
       },
-    });
-    res.send(res.data);
+      {
+        headers: {
+          Authorization: `${process.env.DS_SECRET_TOKEN}`,
+        },
+      }
+    );
+
+    res.send(result.data);
   } catch (e) {
-    res.status(500).send(e);
+    console.log(e);
+    next(e.message);
   }
 });
-
-// auth(user, pass){
-//     return axios.post('http://localhost:3000/auth', {
-//         username: user,
-//         password: pass
-//     })
-// }
-
-// const token = '..your token..'
-
-// axios.post(url, {
-//   //...data
-// }, {
-//   headers: {
-//     'Authorization': `Basic ${token}`
-//   }
-// })
-
-// axios.post(process.env.WORDCLOUD_URL, {
-//     headers: {
-//       Authorization: `${process.env.WORDCLOUD_TOKEN}`
-//     }
-//   }).then(/*Send data to DB using Model Function CREATE*/).catch(err => /*Send Err to FE */)
 
 module.exports = wordCloudRouter;
