90d0d1d693405d30ceb8daec4b74978b21770e34
INFOCORP\Casey.Dietz <caseydietz20@gmail.com>
2021-10-16 13:06:25 -0600

    Updated swagger documentation and added comments to code explaining feature

--- api/wordCloud/wordCloudRouter.js
+++ api/wordCloud/wordCloudRouter.js
@@ -1,8 +1,31 @@
 const { default: axios } = require('axios');
 const wordCloudRouter = require('express').Router();
 
+/**
+ * @swagger
+ * /wordcloud:
+ *  get:
+ *   summary: Attemps to get the wordcloud gif from the data science API.
+ *   security:
+ *     - okta: []
+ *   tags:
+ *     - Word Cloud
+ *   responses:
+ *     200:
+ *       description: Returns a URL that is a gif of a wordcloud.
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: array
+ *             items:
+ *               $ref: '#/components/schemas/WordCloud'
+ *     401:
+ *       description: Unauthorized.
+ */
+
 wordCloudRouter.get('/', async (req, res, next) => {
   try {
+    // assigning result to the POST request to DS API
     const result = await axios.post(
       `${process.env.WORDCLOUD_URL}`,
       {
