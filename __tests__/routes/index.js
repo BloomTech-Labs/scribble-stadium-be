bfb6b430055295b8f8ddc9a1306250aaf7588ba9
Alan Mir <alanpaulphotos@gmail.com>
2021-11-03 22:58:09 -0400

    Deleted submissions folder, and sumbissions test. Also removed the import in test index.js

--- __tests__/routes/index.js
+++ __tests__/routes/index.js
@@ -6,7 +6,6 @@ const StoryTests = require('./stories-test');
 const StoryNewTests = require('./storiesNew-test');
 const ModTests = require('./mod-test');
 const ChildTests = require('./children-test');
-const SubmissionTests = require('./submissions-test');
 const GameTests = require('./game-test');
 const VizTests = require('./viz-tests');
 
