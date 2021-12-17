0496219ebed0aa7c6353d662e7894a550b4dc53a
Durrell Jules <durrelljules@Durrells-Mini.attlocal.net>
2021-12-16 20:48:26 -0500

    changed table to just alter submissions-new, will look into how to drop and rename table

--- data/migrations/20211215202038_Submissions_Table.js
+++ data/migrations/20211215202038_Submissions_Table.js
@@ -1,19 +1,25 @@
 exports.up = function (knex) {
-  return knex.schema
-    .alterTable('Submissions-New', (t) => {
-      t.timestamp('startAt');
-      t.timestamp('readAt');
-      t.timestamp('drawAt');
-      t.timestamp('writeAt');
-      t.timestamp('squadUpAt');
-      t.timestamp('voteAt');
+  return knex.schema.alterTable('Submissions-New', (t) => {
+    t.timestamp('startAt');
+    t.timestamp('readAt');
+    t.timestamp('drawAt');
+    t.timestamp('writeAt');
+    t.timestamp('squadUpAt');
+    t.timestamp('voteAt');
 
-      t.string('gameMode').notNullable();
-    })
-    .dropTable('Submissions-New')
-    .renameTable('Submissions-New', 'Submissions');
+    t.string('gameMode').notNullable();
+  });
 };
 
 exports.down = function (knex) {
-  knex.schema.dropTableIfItExists('Submissions');
+  knex.schema.alterTable('Submissions-New', (t) => {
+    t.dropColumn('startAt');
+    t.dropColumn('readAt');
+    t.dropColumn('drawAt');
+    t.dropColumn('writeAt');
+    t.dropColumn('squadUpAt');
+    t.dropColumn('voteAt');
+
+    t.dropColumn('gameMode');
+  });
 };
