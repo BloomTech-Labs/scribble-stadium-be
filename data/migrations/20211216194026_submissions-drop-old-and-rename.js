a05aa55f92a8b80acf2175553d54afa2d69006a3
Durrell Jules <durrelljules@Durrells-Mini.attlocal.net>
2021-12-17 19:14:02 -0500

    made syntax corrections on the submissions-drop-old-and-rename migration

--- data/migrations/20211216194026_submissions-drop-old-and-rename.js
+++ data/migrations/20211216194026_submissions-drop-old-and-rename.js
@@ -1,112 +1,100 @@
-exports.up = function(knex) {
-    return knex.schema
-    .alterTable("Writing", t=>{
-        t.dropForeign('SubmissionID');
-        t.foreign('SubmissionID')
-        .references('Submissions-New.id');
+exports.up = function (knex) {
+  return knex.schema
+    .alterTable('Writing', (t) => {
+      t.dropForeign('SubmissionID');
+      t.foreign('SubmissionID').references('Submissions-New.id');
     })
-    .alterTable("Drawing", t=>{
-        t.dropForeign('SubmissionID');
-        t.foreign('SubmissionID')
-        .references('Submissions-New.id');
+    .alterTable('Drawing', (t) => {
+      t.dropForeign('SubmissionID');
+      t.foreign('SubmissionID').references('Submissions-New.id');
     })
-    .alterTable("Flags", t=>{
-        t.dropForeign('SubmissionID');
-        t.foreign('SubmissionID')
-        .references('Submissions-New.id');
+    .alterTable('Flags', (t) => {
+      t.dropForeign('SubmissionID');
+      t.foreign('SubmissionID').references('Submissions-New.id');
     })
-    .alterTable("Members", t=>{
-        t.dropForeign('SubmissionID');
-        t.foreign('SubmissionID')
-        .references('Submissions-New.id');
+    .alterTable('Members', (t) => {
+      t.dropForeign('SubmissionID');
+      t.foreign('SubmissionID').references('Submissions-New.id');
     })
-    .alterTable("Points", t=>{
-        t.dropForeign('SubmissionID');
-        t.foreign('SubmissionID')
-        .references('Submissions-New.id');
+    .alterTable('Points', (t) => {
+      t.dropForeign('SubmissionID');
+      t.foreign('SubmissionID').references('Submissions-New.id');
     })
-    .alterTable("Faceoffs", t=>{
-        t.dropForeign('SubmissionID1')
-        t.foreign('SubmissionID1')
-        .references('Submissions-New.id');
+    .alterTable('Faceoffs', (t) => {
+      t.dropForeign('SubmissionID1');
+      t.foreign('SubmissionID1').references('Submissions-New.id');
 
-        t.dropForeign('SubmissionID2')
-        t.foreign('SubmissionID2')
-        .references('Submissions-New.id');
+      t.dropForeign('SubmissionID2');
+      t.foreign('SubmissionID2').references('Submissions-New.id');
     })
     .dropTable('Submissions')
-    .renameTable('Submissions-New', 'Submissions')
+    .renameTable('Submissions-New', 'Submissions');
 };
 
-exports.down = function(knex) {
-    return knex.schema
+exports.down = function (knex) {
+  return knex.schema
     .renameTable('Submissions', 'Submissions-New')
     .createTable('Submissions', (t) => {
-        t.increments('ID');
-        t.integer('ChildID')
-            .notNullable()
-            .unsigned()
-            .references('Children.ID')
-            .onUpdate('CASCADE')
-            .onDelete('RESTRICT');
-        t.integer('StoryID')
-            .notNullable()
-            .unsigned()
-            .references('Stories.ID')
-            .onUpdate('CASCADE')
-            .onDelete('RESTRICT');
-        t.boolean('HasRead').defaultTo(false);
-        t.boolean('HasWritten').defaultTo(false);
-        t.boolean('HasDrawn').defaultTo(false);
-        t.integer('Complexity');
-        t.unique(['ChildID', 'StoryID']);
-        t.boolean('LowConfidence');
-        
+      t.increments('ID');
+      t.integer('ChildID')
+        .notNullable()
+        .unsigned()
+        .references('Children.ID')
+        .onUpdate('CASCADE')
+        .onDelete('RESTRICT');
+      t.integer('StoryID')
+        .notNullable()
+        .unsigned()
+        .references('Stories.ID')
+        .onUpdate('CASCADE')
+        .onDelete('RESTRICT');
+      t.boolean('HasRead').defaultTo(false);
+      t.boolean('HasWritten').defaultTo(false);
+      t.boolean('HasDrawn').defaultTo(false);
+      t.integer('Complexity');
+      t.unique(['ChildID', 'StoryID']);
+      t.boolean('LowConfidence');
 
-        t.enu('Status', null, { enumName:"status", existingType:true, useNative:true
-        }).defaultsTo('CLEAR');
-        t.integer('CohortID')
+      t.enu('Status', null, {
+        enumName: 'status',
+        existingType: true,
+        useNative: true,
+      }).defaultsTo('CLEAR');
+      t.integer('CohortID')
         .notNullable()
         .unsigned()
         .references('Cohorts.ID')
         .onUpdate('CASCADE')
         .onDelete('RESTRICT')
         .defaultsTo(1);
-        t.dropUnique(['ChildID', 'StoryID']);
-        t.unique(['ChildID', 'StoryID', 'CohortID']);
+      t.dropUnique(['ChildID', 'StoryID']);
+      t.unique(['ChildID', 'StoryID', 'CohortID']);
     })
-    .alterTable("Writing", t=>{
-        t.dropForeign('SubmissionID');
-        t.foreign('SubmissionID')
-        .references('Submissions.ID');
+    .alterTable('Writing', (t) => {
+      t.dropForeign('SubmissionID');
+      t.foreign('SubmissionID').references('Submissions.ID');
     })
-    .alterTable("Drawing", t=>{
-        t.dropForeign('SubmissionID');
-        t.foreign('SubmissionID')
-        .references('Submissions.ID');
+    .alterTable('Drawing', (t) => {
+      t.dropForeign('SubmissionID');
+      t.foreign('SubmissionID').references('Submissions.ID');
     })
-    .alterTable("Flags", t=>{
-        t.dropForeign('SubmissionID');
-        t.foreign('SubmissionID')
-        .references('Submissions.ID');
+    .alterTable('Flags', (t) => {
+      t.dropForeign('SubmissionID');
+      t.foreign('SubmissionID').references('Submissions.ID');
     })
-    .alterTable("Members", t=>{
-        t.dropForeign('SubmissionID');
-        t.foreign('SubmissionID')
-        .references('Submissions.ID');
+    .alterTable('Members', (t) => {
+      t.dropForeign('SubmissionID');
+      t.foreign('SubmissionID').references('Submissions.ID');
     })
-    .alterTable("Points", t=>{
-        t.dropForeign('SubmissionID');
-        t.foreign('SubmissionID')
-        .references('Submissions.ID');
+    .alterTable('Points', (t) => {
+      t.dropForeign('SubmissionID');
+      t.foreign('SubmissionID').references('Submissions.ID');
     })
-    .alterTable("Faceoffs", t=>{
-        t.dropForeign('SubmissionID1')
-        t.foreign('SubmissionID1')
-        .references('Submissions.ID');
-        
-        t.dropForeign('SubmissionID2')
-        t.foreign('SubmissionID2')
-        .references('Submissions.ID');
-    })
+    .alterTable('Faceoffs', (t) => {
+      t.dropForeign('SubmissionID1');
+      t.foreign('SubmissionID1').references('Submissions.ID');
+
+      t.dropForeign('SubmissionID2');
+      t.foreign('SubmissionID2').references('Submissions.ID');
+    });
 };
