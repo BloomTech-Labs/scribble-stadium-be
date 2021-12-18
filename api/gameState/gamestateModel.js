8e66b57008b9959dc0cac22321fa66397705c45f
Durrell Jules <durrelljules@Durrells-Mini.attlocal.net>
2021-12-17 21:07:06 -0500

    tested game state router for the GET call, seems to be working but needs to be completed

--- api/gameState/gamestateModel.js
+++ api/gameState/gamestateModel.js
@@ -2,27 +2,30 @@ const db = require('../../data/db-config');
 
 // get game state by KID ID
 
-const getGameStateTimesbyID = (ID) => {
-    return db('Submissions-New as SN')
-      .where('SN.ID', ID)
-      .select([
-        'SN.startAt',
-        'SN.readAt',
-        'SN.drawAt',
-        'SN.writeAt',
-        'SN.squadUpAt',
-        'SN.voteAt',
-        'SN.gameMode'
-      ]);
-  };
+const getGameStateTimesbyID = (id) => {
+  return db('Submissions as S')
+    .where('S.id', id)
+    .select([
+      'S.startAt',
+      'S.readAt',
+      'S.drawAt',
+      'S.writeAt',
+      'S.squadUpAt',
+      'S.voteAt',
+      'S.gameMode',
+    ]);
+};
 
+//pull
+const addGameStateTimesbyID = (id, Data) => {
+  return db('Submissions as S')
+    .where('S.id', id)
+    .insert({
+      ...Data,
+    });
+};
 
-
-  //pull
-  const addGameStateTimesbyID = (ID, Data) => {
-    return db('Submissions-New as SN')
-      .where('SN.ID', ID)
-      .insert({
-          ...Data
-      });
-  };
+module.exports = {
+  addGameStateTimesbyID,
+  getGameStateTimesbyID,
+};
