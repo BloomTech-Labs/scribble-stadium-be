58ab9a9a9b81ae016e494fbf330027869a1b1d92
Chris Lau <chrislyc.lau@gmail.com>
2021-12-22 01:55:15 -0700

    Undoing the changes in seeds by deleting extra files

--- data/seeds/011_Squads.js
+++
@@ -1,9 +0,0 @@
-const squads = [
-  { CohortID: 1, Winner: null },
-  { CohortID: 1, Winner: null },
-];
-
-exports.seed = function (knex) {
-  // Inserts seed entries
-  return knex('Squads').insert(squads);
-};
