c2cba386d5e9659167f8b5c8047f7396de7fee5a
Paul Vigar <Pvigar@gmail.com>
2021-12-23 13:47:07 -0500

    Delete aws.js

--- config/aws.js
+++
@@ -1,9 +0,0 @@
-// /* istanbul ignore file */
-module.exports = {
-   s3: {
-     config: {
-       accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-     },
-   },
- };
