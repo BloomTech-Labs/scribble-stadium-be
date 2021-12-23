93a06f137b9d2cd434334e49194ec8f315902115
Durrell Jules <durrelljules@Durrells-Mini.attlocal.net>
2021-12-23 01:32:28 -0500

    removed fields that overlapped from pr #96, changed order of fields for down migration

--- data/migrations/20211215202038_Submissions_Table.js
+++ data/migrations/20211215202038_Submissions_Table.js
@@ -1,9 +1,5 @@
 exports.up = function (knex) {
   return knex.schema.alterTable('Submissions', (t) => {
-    t.timestamp('startAt');
-    t.timestamp('readAt');
-    t.timestamp('drawAt');
-    t.timestamp('writeAt');
     t.timestamp('squadUpAt');
     t.timestamp('voteAt');
     t.string('gameMode').notNullable();
@@ -12,12 +8,8 @@ exports.up = function (knex) {
 
 exports.down = function (knex) {
   return knex.schema.alterTable('Submissions', (t) => {
-    t.dropColumn('startAt');
-    t.dropColumn('readAt');
-    t.dropColumn('drawAt');
-    t.dropColumn('writeAt');
-    t.dropColumn('squadUpAt');
-    t.dropColumn('voteAt');
     t.dropColumn('gameMode');
+    t.dropColumn('voteAt');
+    t.dropColumn('squadUpAt');
   });
 };
