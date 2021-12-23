6a9d13e3c6bee9e86a548d9c11a5ceb26e85cd40
Paul Vigar <Pvigar@gmail.com>
2021-12-23 13:46:22 -0500

    Update aws.js

--- config/aws.js
+++ config/aws.js
@@ -1,19 +1,9 @@
 // /* istanbul ignore file */
-// module.exports = {
-//   s3: {
-//     config: {
-//       accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-//       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-//     },
-//   },
-// };
-
-
 module.exports = {
-  s3: {
-    config: {
-      accessKeyId: process.env.AWSAccessKeyId,
-      secretAccessKey: process.env.AWSSecretKey,
-    },
-  },
-};
+   s3: {
+     config: {
+       accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+     },
+   },
+ };
