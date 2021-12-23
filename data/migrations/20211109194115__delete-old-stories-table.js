8a293e1e40df9dbffb64b2bdb82c0ec64c73b855
HanselV <hanselviva@gmail.com>
2021-12-22 18:57:12 -0800

    down migration for dropping stories table added. dropping and renaming migrations integrated together

--- data/migrations/20211109194115__delete-old-stories-table.js
+++ data/migrations/20211109194115__delete-old-stories-table.js
@@ -6,9 +6,25 @@ exports.up = function (knex) {
     .alterTable('Cohorts', (t) => {
       t.dropForeign('StoryID');
     })
-    .dropTableIfExists('Stories', () => {});
+    .dropTableIfExists('Stories', () => {})
+    .renameTable('Stories-New', 'Stories');
 };
 
-exports.down = function () {
-  // This is a destructive migration that cannot be rolled back.
+exports.down = function (knex) {
+  return knex.schema
+    .renameTable('Stories', 'Stories-New')
+    .createTable('Stories', (t) => {
+      t.increments('ID');
+      t.string('Title').notNullable();
+      t.string('URL').notNullable();
+      t.string('WritingPrompt').notNullable();
+      t.string('DrawingPrompt').notNullable();
+      t.string('Audiofile').nullable();
+    })
+    .alterTable('Submissions', (t) => {
+      t.foreign('StoryID').references('Stories.ID');
+    })
+    .alterTable('Cohorts', (t) => {
+      t.foreign('StoryID').references('Stories.ID');
+    });
 };
--- data/migrations/20211116201942__rename-storyNew-to-Story.js
+++
@@ -1,7 +0,0 @@
-exports.up = function (knex) {
-  return knex.schema.renameTable('Stories-New', 'Stories');
-};
-
-exports.down = function (knex) {
-  return knex.schema.renameTable('Stories', 'Stories-New');
-};
