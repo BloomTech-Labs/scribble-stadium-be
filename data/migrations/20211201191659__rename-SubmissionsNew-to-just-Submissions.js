338bce0be22b9c0a82e99adf7b6991ae29dc786d
HanselV <hanselviva@gmail.com>
2021-12-22 19:01:55 -0800

    deleted the separate renaming migrations

--- data/migrations/20211201191659__rename-SubmissionsNew-to-just-Submissions.js
+++
@@ -1,7 +0,0 @@
-exports.up = function (knex) {
-  return knex.schema.renameTable('Submissions-New', 'Submissions');
-};
-
-exports.down = function (knex) {
-  return knex.schema.renameTable('Submissions', 'Submissions-New');
-};
