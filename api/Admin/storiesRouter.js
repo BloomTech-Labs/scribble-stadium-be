8fcbae901f1d77741d14ff22ce7e3f9a9e264af3
miketillman95 <matillman95@gmail.com>
2021-12-28 23:14:33 -0500

    finished add, remove, update router and model

--- api/Admin/storiesRouter.js
+++ api/Admin/storiesRouter.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
 const Stories = require('./storiesModel')
 
+
+
 router.get('/', (req, res) => {
     Stories.getAll(req.query)
     .then( stories => {
@@ -25,8 +27,71 @@ router.get(':id', (req, res) => {
             })
         }
     })
+    .catch( err => {
+        res.status(500).json({
+            message: 'Error retrieving stories from server'
+        })
+    })
 
 })
+
+router.post('/', (req, res) => {
+    Stories.add(req.body)
+        .then(stories =>{
+            res.status(201).json(stories)
+
+        })
+        .catch( err => {
+            res.status(500).json({
+                message: 'Trouble adding stories'
+            })
+        })
+})
+
+router.put('/:id', (req, res) => {
+    const changes = req.body
+    Stories.update(req.params.id, changes)
+        .then(stories => {
+            if(stories){
+                res.status(200).json({
+                    message: 'Stories have been updated'
+                })
+            }
+            else{
+                res.status(404).json({
+                    message: 'Stories could not be found'
+                })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'Error updating the recipe'
+            })
+        })
+})
+router.delete(':id', (req,res) => {
+    Stories.remove(req.params.id)
+        .then(count => {
+            if(count > 0){
+                res.status(200).json({
+                    message: "Story has been deleted"
+                })
+            }else{
+                res.status(404).json({
+                    message: 'Story could not be found'
+                })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'Error deleting the story'
+            })
+        })
+
+        
+})
+
+
 
 
 
