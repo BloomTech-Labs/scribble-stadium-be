7cf295beeccb69fccb43bf684ce99d70877290b2
Matthew Casey <mattcasey879@gmail.com>
2022-02-03 13:21:19 -0600

    Feature/add admins table (#103)
    * made admins table
    
    * down function drops admins table
    
    * created seed data to populate admins table

---
+++ data/migrations/20220202104318_admin-table.js
@@ -0,0 +1,11 @@
+exports.up = function (knex) {
+  return knex.schema.createTable('Admins', (t) => {
+    t.increments('ID');
+    t.string('Email').notNullable().unique();
+    t.string('Name');
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('Admins');
+};
