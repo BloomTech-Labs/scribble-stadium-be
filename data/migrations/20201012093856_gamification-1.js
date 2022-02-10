3162e587f2c3b27b96749fdc3b197bf3b321a739
Kyle <kyle.e.cardwell@gmail.com>
2022-02-09 21:52:15 -0700

    removed CohortID column from Squads table in gamification-1.js

--- data/migrations/20201012093856_gamification-1.js
+++ data/migrations/20201012093856_gamification-1.js
@@ -3,12 +3,6 @@ exports.up = function (knex) {
     .createTable('Squads', (t) => {
       // Squads table just creates a point of reference to connect teams
       t.increments('ID');
-      t.integer('CohortID')
-        .notNullable()
-        .unsigned()
-        .references('Cohorts.ID')
-        .onUpdate('CASCADE')
-        .onDelete('RESTRICT');
     })
     .createTable('Teams', (t) => {
       // A reference that connects team members to their squad
