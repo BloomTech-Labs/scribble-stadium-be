08bb3b7806d910d2303578fa13d0802f47b1be83
Matthew Casey <mattcasey879@gmail.com>
2022-02-17 08:57:37 -0600

    removed Admins Table from schema

--- data/migrations/20220217084904_Remove-admins-and-add-Profiles.js
+++ data/migrations/20220217084904_Remove-admins-and-add-Profiles.js
@@ -1,10 +1,12 @@
 exports.up = function (knex) {
-  return knex.schema.createTable('Profiles', (t) => {
-    t.increments('ID');
-    t.string('Name');
-    t.string('Email');
-    t.string('Type');
-  });
+  return knex.schema
+    .createTable('Profiles', (t) => {
+      t.increments('ID');
+      t.string('Name');
+      t.string('Email');
+      t.string('Type');
+    })
+    .dropTableIfExists('Admins');
 };
 
 exports.down = function (knex) {};
