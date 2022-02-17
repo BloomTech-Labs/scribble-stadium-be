94ac49975e9b39c92f7767f7daac8577da682019
Matthew Casey <mattcasey879@gmail.com>
2022-02-17 08:52:42 -0600

    Made Profiles table

---
+++ data/migrations/20220217084904_Remove-admins-and-add-Profiles.js
@@ -0,0 +1,10 @@
+exports.up = function (knex) {
+  return knex.schema.createTable('Profiles', (t) => {
+    t.increments('ID');
+    t.string('Name');
+    t.string('Email');
+    t.string('Type');
+  });
+};
+
+exports.down = function (knex) {};
