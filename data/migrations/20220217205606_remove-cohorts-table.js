0bd727362bab6a2dfd336eb0aaf19b3c9d38505a
Kyle <kyle.e.cardwell@gmail.com>
2022-02-17 21:46:34 -0700

    updated migration to delete cohorts table and all foreign keys that reference it

--- data/migrations/20220217205606_remove-cohorts-table.js
+++ data/migrations/20220217205606_remove-cohorts-table.js
@@ -1,6 +1,40 @@
 exports.up = function (knex) {
+  return knex.schema
+    .alterTable('Squads', (t) => {
+      t.dropForeign('CohortID');
+    })
+    .alterTable('Children', (t) => {
+      t.dropForeign('CohortID');
+    })
+    .dropTableIfExists('Cohorts');
 };
 
 exports.down = function (knex) {
-  
+  return knex.schema
+    .createTable('Cohorts', (t) => {
+      t.increments('ID');
+      t.integer('StoryID')
+        .notNullable()
+        .unsigned()
+        .references('Stories.ID')
+        .onUpdate('CASCADE')
+        .onDelete('RESTRICT');
+    })
+    .table('Children', (t) => {
+      t.integer('CohortID')
+        .notNullable()
+        .unsigned()
+        .references('CohortID')
+        .onUpdate('CASCADE')
+        .onDelete('RESTRICT')
+        .defaultsTo(1);
+    })
+    .alterTable('Squads', (t) => {
+      t.integer('CohortID')
+        .notNullable()
+        .unsigned()
+        .references('CohortID')
+        .onUpdate('CASCADE')
+        .onDelete('RESTRICT');
+    });
 };
