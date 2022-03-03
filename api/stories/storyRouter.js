508f7e38355d87185ec134d088d6417ce7b98748
Isaac Howard <howardisaac100@gmail.com>
2022-03-02 18:33:33 -0800

    Bug/naming conventions problems (#106)
    * Deleted old unused storyRouter and storyModel
    
    * Changed "new" version of story model and router to better names
    
    * Changed app.js story router import to be more accurate
    
    Kept old 'storyNew' and 'storiesNew' routes, in case they were already in use
    
    * Forgot to save and push earlier; simply updates storyModel name in storyRouter
    
    * Fixed small typo with using the storyRouter

--- api/stories/storyRouter.js
+++ api/stories/storyRouter.js
@@ -1,209 +1,27 @@
 const router = require('express').Router();
-
-const {
-  authRequired,
-  storyValidation,
-  storyUpdateValidation,
-} = require('../middleware');
 const { crudOperationsManager } = require('../../lib');
-
 const Stories = require('./storyModel');
 
-/**
- * Schemas for story data types.
- * @swagger
- * components:
- *  schemas:
- *    Story:
- *      type: object
- *      properties:
- *        Title:
- *          type: string
- *          decription: Title of the story
- *        URL:
- *          type: string
- *          description: URL to the PDF file of the story
- *        WritingPrompt:
- *          type: string
- *          description: Short 1-3 sentence writing prompt
- *        DrawingPrompt:
- *          type: string
- *          description: Short 1-3 sentence drawing prompt
- *      example:
- *        Title: 'Studies in Modern Movement'
- *        URL: 'http://www.someurl.com'
- *        WritingPrompt: 'Write something about the story you just read.'
- *        DrawingPrompt: 'Draw something that happened in the story you just read.'
- *    PostStory:
- *      allOf:
- *        - $ref: '#/components/schemas/Story'
- *        - type: object
- *          required:
- *            - Title
- *            - URL
- *            - WritingPrompt
- *            - DrawingPrompt
- *    GetStory:
- *      allOf:
- *        - type: object
- *          required:
- *            - ID
- *          properties:
- *            ID:
- *              type: integer
- *              readOnly: true
- *              description: Auto-incrementing primary key
- *          example:
- *            ID: 1
- *        - $ref: '#/components/schemas/PostStory'
- *
- *  parameters:
- *    storyId:
- *      name: ID
- *      in: path
- *      description: The unique ID of a story object.
- *      example: 1
- *      schema:
- *        type: integer
- *    cohortQuery:
- *      name: cohortId
- *      in: query
- *      description: a query parameter that tells the server the cohort to search by
- *      example: ?cohortId=1
- *      schema:
- *        type: integer
- */
-
-/**
- * @swagger
- * /story?cohortId={id}:
- *  get:
- *    summary: Returns the story for the given cohort.
- *    security:
- *      - okta: []
- *    tags:
- *      - Stories
- *    parameters:
- *      - $ref: '#/components/parameters/cohortQuery'
- *    responses:
- *      200:
- *        description: Returns the current story for the given cohort.
- *        content:
- *          application/json:
- *            schema:
- *              $ref: '#/components/schemas/GetStory'
- *      401:
- *        $ref: '#/components/responses/UnauthorizedError'
- *      404:
- *        $ref: '#/components/responses/NotFound'
- *      500:
- *        $ref: '#/components/responses/DatabaseError'
- */
-router.get('/', authRequired, async (req, res) => {
-  const cohortId = req.query.cohortId;
-
-  crudOperationsManager.getById(res, Stories.getByCohortId, 'Story', cohortId);
-});
-
-/**
- * @swagger
- * /stories/{id}:
- *  get:
- *    summary: Attempts to query the database for a story with the given ID.
- *    security:
- *      - okta: []
- *    tags:
- *      - Stories
- *    parameters:
- *      - $ref: '#/components/parameters/storyId'
- *    responses:
- *      200:
- *        description: Returns the requested story object.
- *        content:
- *          application/json:
- *            schema:
- *              $ref: '#/components/schemas/GetStory'
- *      401:
- *        $ref: '#/components/responses/UnauthorizedError'
- *      404:
- *        $ref: '#/components/responses/NotFound'
- *      500:
- *        $ref: '#/components/responses/DatabaseError'
- */
-router.get('/:id', authRequired, (req, res) => {
-  // Pull story ID out of the URL params
-  const { id } = req.params;
-
-  crudOperationsManager.getById(res, Stories.getById, 'Story', id);
-});
-
-/**
- * @swagger
- * /story:
- *  post:
- *    summary: Attempts to add a new story to the database.
- *    security:
- *      - okta: []
- *    tags:
- *      - Stories
- *    requestBody:
- *      description: Object to be added to the Stories table.
- *      content:
- *        application/json:
- *          schema:
- *            $ref: '#/components/schemas/PostStory'
- *    responses:
- *      201:
- *        description: Returns the ID of the newly created story.
- *        content:
- *          application/json:
- *            example: 1
- *            schema:
- *              type: integer
- *      400:
- *        $ref: '#/components/responses/InvalidFormat'
- *      401:
- *        $ref: '#/components/responses/UnauthorizedError'
- *      500:
- *        $ref: '#/components/responses/DatabaseError'
- */
-router.post('/', authRequired, storyValidation, async (req, res) => {
+router.post('/', (req, res) => {
   // Pull relevant data out of the request object
   const newStory = req.body;
 
   crudOperationsManager.post(res, Stories.add, 'Story', newStory);
 });
 
-/**
- * @swagger
- * /story/{id}:
- *  put:
- *    summary: Attempts to update the story with the given ID parameter.
- *    security:
- *      - okta: []
- *    tags:
- *      - Stories
- *    parameters:
- *      - $ref: '#/components/parameters/storyId'
- *    requestBody:
- *      description: Changes to be applied to the specified story.
- *      content:
- *        application/json:
- *          schema:
- *            $ref: '#/components/schemas/Story'
- *    responses:
- *      204:
- *        $ref: '#/components/responses/EmptySuccess'
- *      400:
- *        $ref: '#/components/responses/InvalidFormat'
- *      401:
- *        $ref: '#/components/responses/UnauthorizedError'
- *      404:
- *        $ref: '#/components/responses/NotFound'
- *      500:
- *        $ref: '#/components/responses/DatabaseError'
- */
-router.put('/:id', authRequired, storyUpdateValidation, (req, res) => {
+//This endpoint returns all stories from 'Stories-New' table with corresponding 'Episodes' attached
+router.get('/', (req, res) => {
+  crudOperationsManager.getAll(res, Stories.getAllStories, 'allStories');
+});
+
+router.get('/:id', (req, res) => {
+  // Pull story ID out of the URL params
+  const { id } = req.params;
+
+  crudOperationsManager.getById(res, Stories.getById, 'Story', id);
+});
+
+router.put('/:id', (req, res) => {
   // Pull relevant data out of the request object
   const { id } = req.params;
   const changes = req.body;
@@ -211,32 +29,76 @@ router.put('/:id', authRequired, storyUpdateValidation, (req, res) => {
   crudOperationsManager.update(res, Stories.update, 'Story', id, changes);
 });
 
-/**
- * @swagger
- * /story/{id}:
- *  delete:
- *    summary: Attempts to delete the story with the specified ID.
- *    security:
- *      - okta: []
- *    tags:
- *      - Stories
- *    parameters:
- *      - $ref: '#/components/parameters/storyId'
- *    responses:
- *      204:
- *        $ref: '#/components/responses/EmptySuccess'
- *      401:
- *        $ref: '#/components/responses/UnauthorizedError'
- *      404:
- *        $ref: '#/components/responses/NotFound'
- *      500:
- *        $ref: '#/components/responses/DatabaseError'
- */
-router.delete('/:id', authRequired, (req, res) => {
+router.delete('/:id', (req, res) => {
   // Pull story ID out of the URL params
   const { id } = req.params;
 
   crudOperationsManager.update(res, Stories.remove, 'Story', id);
+});
+
+//Does not get episode if reading/writing prompts do not exist.
+
+router.get('/episodes/:eid', async (req, res) => {
+  try {
+    // Pull episode ID out of the URL params
+    const { eid } = req.params;
+    const episode = await Stories.getEpisodeByID(eid);
+    const prompts = await Stories.getPromptsByEpisodeID(eid);
+    const data = {
+      ID: episode[0].ID,
+      StoryID: episode[0].StoryID,
+      EpisodeNumber: episode[0].EpisodeNumber,
+      TextURL: episode[0].TextURL,
+      AudioURL: episode[0].AudioURL,
+      WritingPrompt: prompts[0].WritingPrompt.Prompt,
+      DrawingPrompt: prompts[0].DrawingPrompt.Prompt,
+    };
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(404).json({
+      message: 'EpisodeNotFound',
+    });
+  }
+});
+
+//Work in progress by next cohort to add constraint to EpisodeNumber to be unique per story ID
+router.post('/episodes', (req, res) => {
+  // Pull relevant data out of the request object
+  const { StoryID, EpisodeNumber, TextURL, AudioURL } = req.body;
+  const newEpisode = {
+    StoryID: StoryID,
+    EpisodeNumber: EpisodeNumber,
+    TextURL: TextURL,
+    AudioURL: AudioURL,
+  };
+  crudOperationsManager.post(res, Stories.addEpisode, 'Story', newEpisode);
+});
+
+router.put('/episodes/:eid', (req, res) => {
+  // Pull episode ID out of the URL params
+  const { eid } = req.params;
+  // Pull relevant data out of the request object
+  const { EpisodeNumber, TextURL, AudioURL } = req.body;
+  const changes = {
+    EpisodeNumber: EpisodeNumber,
+    TextURL: TextURL,
+    AudioURL: AudioURL,
+  };
+
+  crudOperationsManager.update(
+    res,
+    Stories.updateEpisode,
+    'Episode',
+    eid,
+    changes
+  );
+});
+
+router.delete('/episodes/:eid', (req, res) => {
+  // Pull episode ID out of the URL params
+  const { eid } = req.params;
+
+  crudOperationsManager.update(res, Stories.removeEpisode, 'Episode', eid);
 });
 
 module.exports = router;
